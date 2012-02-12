a80785397addc464cb6b1ae3826279338de86e4b
Vincent Jacques <vincent@vincent-jacques.net>
2012-02-12 19:38:53 +0100

    Repository

--- IntegrationTest.py
+++ IntegrationTest.py
@@ -7,8 +7,10 @@ g = Github( GithubCredentials.login, GithubCredentials.password )
 
 u = g.get_user()
 print u.login, u.name
+print u.get_repos()
 print
 
 u = g.get_user( "nvie" )
 print u.login, u.name
+print u.get_repos()
 print
--- github/User.py
+++ github/User.py
@@ -8,7 +8,7 @@ AuthenticatedUser = GithubObject(
         "blog", "location", "email", "hireable", "bio", "public_repos",
         "public_gists", "followers", "following", "html_url", "created_at",
         "type", "total_private_repos", "owned_private_repos", "private_gists",
-        "disk_usage", "collaborators", "plan"
+        "disk_usage", "collaborators", "plan",
     ),
 )
 
@@ -19,7 +19,24 @@ NamedUser = GithubObject(
         "login", "id", "avatar_url", "gravatar_id", "url", "name", "company",
         "blog", "location", "email", "hireable", "bio", "public_repos",
         "public_gists", "followers", "following", "html_url", "created_at",
-        "type", "total_private_repos", "owned_private_repos", "private_gists",
-        "disk_usage", "collaborators", "plan"
+        "type",
     ),
 )
+
+Repository = GithubObject(
+    "Repository",
+    BaseUrl( lambda obj: "/repos/" + obj.owner.login + "/" + obj.name ),
+    SimpleScalarAttributes(
+        "url", "html_url", "clone_url", "git_url", "ssh_url", "svn_url",
+        "name", "description", "homepage", "language", "private",
+        "fork", "forks", "watchers", "size", "master_branch", "open_issues",
+        "pushed_at", "created_at", "organization", "parent", "source",
+        "has_issues", "has_wiki", "has_downloads",
+        # Not documented
+        "mirror_url", "updated_at", "id",
+    ),
+    ExtendedScalarAttribute( "owner", NamedUser ),
+)
+
+AuthenticatedUser._addAttributePolicy( ExtendedListAttribute( "repos", Repository ) )
+NamedUser._addAttributePolicy( ExtendedListAttribute( "repos", Repository ) )
