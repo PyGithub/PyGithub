79a8c630961c21f28cb5354e62f03479b2e27d84
Vincent Jacques <vincent@vincent-jacques.net>
2012-02-26 09:00:16 +0000

    Typo

--- ReadMe.md
+++ ReadMe.md
@@ -21,7 +21,7 @@ First create a Gihub instance:
 
 Then play with your Github objects:
 
-    for repo in g.user().get_repos():
+    for repo in g.get_user().get_repos():
         print repo.name
         repo.edit( has_wiki = False )
 
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
 
         Then play with your Github objects::
 
-            for repo in g.user().get_repos():
+            for repo in g.get_user().get_repos():
                 print repo.name
                 repo.edit( has_wiki = False )
 
