26676020797823c0ff91ccb3cca6112d4b93261e
Vincent Jacques <vincent@vincent-jacques.net>
2012-05-08 10:49:43 +0100

    First recording for new integration tests

--- NewIntegrationTest.py
+++ NewIntegrationTest.py
@@ -85,19 +85,26 @@ class TestCase( unittest.TestCase ):
     def setUp( self ):
         unittest.TestCase.setUp( self )
         self.__file = None
-        httplib.HTTPSConnection = lambda *args, **kwds: ReplayingHttpsConnection( self.__openFile() )
+        httplib.HTTPSConnection = lambda *args, **kwds: ReplayingHttpsConnection( self.__openFile( "r" ) )
         self.g = github.Github( "login", "password" )
+
+    def setUpForRecord( self ):
+        import GithubCredentials
+        unittest.TestCase.setUp( self )
+        self.__file = None
+        httplib.HTTPSConnection = lambda *args, **kwds: RecordingHttpsConnection( self.__openFile( "w" ), *args, **kwds )
+        self.g = github.Github( GithubCredentials.login, GithubCredentials.password )
 
     def tearDown( self ):
         unittest.TestCase.tearDown( self )
         self.__file.close()
 
-    def __openFile( self ):
+    def __openFile( self, mode ):
         for ( _, _, functionName, _ ) in traceback.extract_stack():
             if functionName.startswith( "test" ):
                 fileName = os.path.join( "ReplayDataForNewIntegrationTest", self.__class__.__name__ + "." + functionName[ 4: ] + ".txt" )
         if self.__file is None:
-            self.__file = open( fileName )
+            self.__file = open( fileName, mode )
         return self.__file
 
 class AuthenticatedUser( TestCase ):
@@ -115,9 +122,9 @@ class AuthenticatedUser( TestCase ):
         self.assertEqual( self.u.collaborators, 0 )
         self.assertEqual( self.u.company, "Criteo" )
         self.assertEqual( self.u.created_at, "2010-07-09T06:10:06Z" )
-        self.assertEqual( self.u.disk_usage, 17252 )
+        self.assertEqual( self.u.disk_usage, 16692 )
         self.assertEqual( self.u.email, "vincent@vincent-jacques.net" )
-        self.assertEqual( self.u.followers, 12 )
+        self.assertEqual( self.u.followers, 13 )
         self.assertEqual( self.u.following, 24 )
         self.assertEqual( self.u.gravatar_id, "b68de5ae38616c296fa345d2b9df2225" )
         self.assertEqual( self.u.hireable, False )
@@ -128,14 +135,25 @@ class AuthenticatedUser( TestCase ):
         self.assertEqual( self.u.name, "Vincent Jacques" )
         self.assertEqual( self.u.owned_private_repos, 5 )
         ### @todo self.assertEqual( self.u.plan, "" )
-        self.assertEqual( self.u.private_gists, 2 )
-        self.assertEqual( self.u.public_gists, 4 )
-        self.assertEqual( self.u.public_repos, 15 )
+        self.assertEqual( self.u.private_gists, 5 )
+        self.assertEqual( self.u.public_gists, 1 )
+        self.assertEqual( self.u.public_repos, 10 )
         self.assertEqual( self.u.total_private_repos, 5 )
         self.assertEqual( self.u.type, "User" )
         self.assertEqual( self.u.url, "https://api.github.com/users/jacquev6" )
 
 if len( sys.argv ) > 1:
-    pass
+    assert sys.argv[ 1 ] == "--record"
+    for method in sys.argv[ 2 : ]:
+        class_, method = method.split( "." )
+        method = "test" + method
+        print "Recording method", method, "of class", class_
+        # exec "testCase = " + class_ + "()"
+        testCase = AuthenticatedUser( methodName = method )
+        method = getattr( testCase, method )
+        TestCase.setUp = TestCase.setUpForRecord
+        testCase.setUp()
+        method()
+        testCase.tearDown()
 else:
     unittest.main()
