114db51fe79cad37e50bc3bc82c183c7d2845316
Vincent Jacques <vincent@vincent-jacques.net>
2012-05-09 18:13:08 +0200

    Test attributes of Repository

--- NewIntegrationTest.py
+++ NewIntegrationTest.py
@@ -84,6 +84,7 @@ class ReplayingHttpsConnection:
 class TestCase( unittest.TestCase ):
     def setUp( self ):
         unittest.TestCase.setUp( self )
+        self.__fileName = ""
         self.__file = None
         httplib.HTTPSConnection = lambda *args, **kwds: ReplayingHttpsConnection( self.__openFile( "r" ) )
         self.g = github.Github( "login", "password" )
@@ -97,8 +98,7 @@ class TestCase( unittest.TestCase ):
 
     def tearDown( self ):
         unittest.TestCase.tearDown( self )
-        self.assertEqual( self.__file.readline(), "" )
-        self.__file.close()
+        self.__closeReplayFileIfNeeded()
 
     def tearDownForRecord( self ):
         unittest.TestCase.tearDown( self )
@@ -106,11 +106,18 @@ class TestCase( unittest.TestCase ):
 
     def __openFile( self, mode ):
         for ( _, _, functionName, _ ) in traceback.extract_stack():
-            if functionName.startswith( "test" ):
-                fileName = os.path.join( "ReplayDataForNewIntegrationTest", self.__class__.__name__ + "." + functionName[ 4: ] + ".txt" )
-        if self.__file is None:
-            self.__file = open( fileName, mode )
+            if functionName.startswith( "test" ) or functionName == "setUp" or functionName == "tearDown":
+                fileName = os.path.join( "ReplayDataForNewIntegrationTest", self.__class__.__name__ + "." + functionName + ".txt" )
+        if fileName != self.__fileName:
+            self.__closeReplayFileIfNeeded()
+            self.__fileName = fileName
+            self.__file = open( self.__fileName, mode )
         return self.__file
+
+    def __closeReplayFileIfNeeded( self ):
+        if self.__file is not None:
+            self.assertEqual( self.__file.readline(), "" )
+            self.__file.close()
 
 class AuthenticatedUser( TestCase ):
     def setUp( self ):
@@ -193,10 +200,49 @@ class AuthenticatedUser( TestCase ):
         self.assertEqual( self.u.hireable, oldHireable )
         self.assertEqual( self.u.bio, oldBio )
 
+class Repository( TestCase ):
+    def setUp( self ):
+        TestCase.setUp( self )
+        self.r = self.g.get_user().get_repo( "PyGithub" )
+
+    def tearDown( self ):
+        TestCase.tearDown( self )
+
+    def testAttributes( self ):
+        self.assertEqual( self.r.clone_url, "https://github.com/jacquev6/PyGithub.git" )
+        self.assertEqual( self.r.created_at, "2012-02-25T12:53:47Z" )
+        self.assertEqual( self.r.description, "Python library implementing the full Github API v3" )
+        self.assertEqual( self.r.fork, False )
+        self.assertEqual( self.r.forks, 2 )
+        self.assertEqual( self.r.git_url, "git://github.com/jacquev6/PyGithub.git" )
+        self.assertEqual( self.r.has_downloads, True )
+        self.assertEqual( self.r.has_issues, True )
+        self.assertEqual( self.r.has_wiki, False )
+        self.assertEqual( self.r.homepage, "http://vincent-jacques.net/PyGithub" )
+        self.assertEqual( self.r.html_url, "https://github.com/jacquev6/PyGithub" )
+        self.assertEqual( self.r.id, 3544490 )
+        self.assertEqual( self.r.language, "Python" )
+        self.assertEqual( self.r.master_branch, None ) ### @todo Why does this trigger a new request to github ?
+        self.assertEqual( self.r.mirror_url, None )
+        self.assertEqual( self.r.name, "PyGithub" )
+        self.assertEqual( self.r.open_issues, 15 )
+        self.assertEqual( self.r.organization, None )
+        self.assertEqual( self.r.owner.login, "jacquev6" )
+        self.assertEqual( self.r.parent, None )
+        self.assertEqual( self.r.permissions, { "admin": True, "pull": True, "push": True } ) ### @todo Create a Permission class
+        self.assertEqual( self.r.private, False )
+        self.assertEqual( self.r.pushed_at, "2012-05-08T19:27:43Z" )
+        self.assertEqual( self.r.size, 212 )
+        self.assertEqual( self.r.source, None )
+        self.assertEqual( self.r.ssh_url, "git@github.com:jacquev6/PyGithub.git" )
+        self.assertEqual( self.r.svn_url, "https://github.com/jacquev6/PyGithub" )
+        self.assertEqual( self.r.updated_at, "2012-05-08T19:27:43Z" )
+        self.assertEqual( self.r.url, "https://api.github.com/repos/jacquev6/PyGithub" )
+        self.assertEqual( self.r.watchers, 13 )
+
 if len( sys.argv ) > 1 and sys.argv[ 1 ] == "--record":
     for method in sys.argv[ 2 : ]:
         class_, method = method.split( "." )
-        method = "test" + method
         print "Recording method", method, "of class", class_
         testCase = eval( class_ )( methodName = method )
         method = getattr( testCase, method )
