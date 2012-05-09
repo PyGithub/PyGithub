1292bf0e22c796e91cc3d6e24b544aece8c21f2a
Vincent Jacques <vincent@vincent-jacques.net>
2012-05-09 18:22:33 +0200

    Remove completion functions from GitAuthor

--- github/GithubObjects/GitAuthor.py
+++ github/GithubObjects/GitAuthor.py
@@ -14,43 +14,23 @@ class GitAuthor( object ):
         self.__completed = False
         self.__initAttributes()
         self.__useAttributes( attributes )
-        if not lazy:
-            self.__complete()
 
     @property
     def date( self ):
-        self.__completeIfNeeded( self.__date )
         return self.__date
 
     @property
     def email( self ):
-        self.__completeIfNeeded( self.__email )
         return self.__email
 
     @property
     def name( self ):
-        self.__completeIfNeeded( self.__name )
         return self.__name
 
     def __initAttributes( self ):
         self.__date = None
         self.__email = None
         self.__name = None
-
-    def __completeIfNeeded( self, testedAttribute ):
-        if not self.__completed and testedAttribute is None:
-            self.__complete()
-
-    # @todo Do not generate __complete if type has no url attribute
-    def __complete( self ):
-        status, headers, data = self.__requester.request(
-            "GET",
-            self.__url,
-            None,
-            None
-        )
-        self.__useAttributes( data )
-        self.__completed = True
 
     def __useAttributes( self, attributes ):
          #@todo No need to check if attribute is in attributes when attribute is mandatory
