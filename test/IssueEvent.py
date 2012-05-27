4aadfff21cdd2d2566b0e4bd7309c233b5f4ae23
Vincent Jacques <vincent@vincent-jacques.net>
2012-05-27 08:33:23 +0100

    Improve test coverage

--- test/IssueEvent.py
+++ test/IssueEvent.py
@@ -3,13 +3,13 @@ import Framework
 class IssueEvent( Framework.TestCase ):
     def setUp( self ):
         Framework.TestCase.setUp( self )
-        self.event = self.g.get_user().get_repo( "PyGithub" ).get_issues_event( 15819975 )
+        self.event = self.g.get_user().get_repo( "PyGithub" ).get_issues_event( 16348656 )
 
     def testAttributes( self ):
         self.assertEqual( self.event.actor.login, "jacquev6" )
-        self.assertEqual( self.event.commit_id, None )
-        self.assertEqual( self.event.created_at, "2012-05-19T10:38:23Z" )
-        self.assertEqual( self.event.event, "subscribed" )
-        self.assertEqual( self.event.id, 15819975 )
-        self.assertEqual( self.event.issue.number, 28 )
-        self.assertEqual( self.event.url, "https://api.github.com/repos/jacquev6/PyGithub/issues/events/15819975" )
+        self.assertEqual( self.event.commit_id, "ed866fc43833802ab553e5ff8581c81bb00dd433" )
+        self.assertEqual( self.event.created_at, "2012-05-27T07:29:25Z" )
+        self.assertEqual( self.event.event, "referenced" )
+        self.assertEqual( self.event.id, 16348656 )
+        self.assertEqual( self.event.issue.number, 30 )
+        self.assertEqual( self.event.url, "https://api.github.com/repos/jacquev6/PyGithub/issues/events/16348656" )
