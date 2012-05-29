72166302fce622bb542f785935c312af252c2721
Vincent Jacques <vincent@vincent-jacques.net>
2012-05-29 19:49:26 +0100

    Add a test for the 950 closed issues refered by issue #33

--- test/Issue33.py
+++ test/Issue33.py
@@ -7,3 +7,6 @@ class Issue33( Framework.TestCase ): # https://github.com/jacquev6/PyGithub/issu
 
     def testOpenIssues( self ):
         self.assertEqual( len( list( self.repo.get_issues() ) ), 338 )
+
+    def testClosedIssues( self ):
+        self.assertEqual( len( list( self.repo.get_issues( state = "closed" ) ) ), 950 )
