41c2b94f95505133bba0e4d4bf3eccc3f242ac3d
Vincent Jacques <vincent@vincent-jacques.net>
2012-06-03 19:56:33 +0100

    Add a copyright and license notice (issue #16)

--- codegen/generate.py
+++ codegen/generate.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+# Copyright 2012 Vincent Jacques
+# vincent@vincent-jacques.net
+
+# This file is part of PyGithub. http://vincent-jacques.net/PyGithub
+
+# PyGithub is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# PyGithub is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License along with PyGithub.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import json
 import itertools
@@ -58,6 +71,9 @@ for class_ in description[ "classes" ]:
                 code.append( "" )
             if line.startswith( "class" ):
                 code.append( "" )
+            if line == "##########":
+                code.append( "" )
+                continue
             code.append( line )
         f.write( "\n".join( code ) + "\n" )
 
