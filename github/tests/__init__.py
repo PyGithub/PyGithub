c0ccafa7ec02af00d5f59085e1bf5e99c74a7aee
Vincent Jacques <vincent@vincent-jacques.net>
2012-10-28 18:51:06 +0100

    Restore Python 2.5 and 2.6?
    (This commit was 'push --force'd)

--- github/tests/__init__.py
+++ github/tests/__init__.py
@@ -11,10 +11,20 @@
 
 # You should have received a copy of the GNU Lesser General Public License along with PyGithub.  If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
+import sys
+
+atLeastPython27 = sys.hexversion >= 0x02070000
+
+if atLeastPython27:
+    import unittest
+else:  # pragma no cover
+    import unittest2 as unittest  # pragma no cover
 
 import AllTests
 
 
 def run():
-    return unittest.main(module=AllTests, argv=["Dummy Script Name"], exit=False).result
+    testLoader = unittest.loader.TestLoader()
+    testRunner = unittest.runner.TextTestRunner(verbosity=1)
+    test = testLoader.loadTestsFromModule(AllTests)
+    return testRunner.run(test)
