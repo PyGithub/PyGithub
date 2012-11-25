4b5672b33886ebc7c9c22d12a8ba30feaa60ec3f
Vincent Jacques <vincent@vincent-jacques.net>
2012-11-25 15:30:06 +0100

    Do not push docs

---
+++ make_doc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+rm -rf doc/build
+git clone . doc/build/html
+cd doc/build/html
+git checkout generated-doc
+git rm -rf .
+
+sphinx-build -b html -d ../../build/doctrees ../../source .
+
+git add .
+git commit --message "Automatic generation" || echo
+git checkout gh-pages
+( git merge generated-doc --message "Update Github pages with generated doc" && git push origin generated-doc gh-pages ) || echo "Something went wrong. cd doc/build/html; git mergetool; git commit; git push"
