ce3fb533e06f5973813f845284129125718b83ae
David Farr <david.farr@sap.com>
2013-12-16 16:24:24 -0800

    adding the tests for issue 214

--- github/tests/Issue214.py
+++ github/tests/Issue214.py
@@ -4,7 +4,6 @@
 #                                                                              #
 # Copyright 2012 Vincent Jacques <vincent@vincent-jacques.net>                 #
 # Copyright 2012 Zearin <zearin@gonk.net>                                      #
-# Copyright 2013 AKFish <akfish@gmail.com>                                     #
 # Copyright 2013 Vincent Jacques <vincent@vincent-jacques.net>                 #
 #                                                                              #
 # This file is part of PyGithub. http://jacquev6.github.com/PyGithub/          #
@@ -24,66 +23,48 @@
 #                                                                              #
 ################################################################################
 
-from AuthenticatedUser import *
-from Authentication import *
-from Authorization import *
-from Branch import *
-from Commit import *
-from CommitComment import *
-from CommitStatus import *
-from ContentFile import *
-from Download import *
-from Event import *
-from Gist import *
-from GistComment import *
-from GitBlob import *
-from GitCommit import *
-from Github_ import *
-from GitRef import *
-from GitTag import *
-from GitTree import *
-from Hook import *
-from Issue import *
-from IssueComment import *
-from IssueEvent import *
-from Label import *
-from Milestone import *
-from NamedUser import *
-from Markdown import *
-from Organization import *
-from PullRequest import *
-from PullRequestComment import *
-from PullRequestFile import *
-from RateLimiting import *
-from Repository import *
-from RepositoryKey import *
-from Status import *
-from Tag import *
-from Team import *
-from UserKey import *
+import github
 
-from PaginatedList import *
-from Exceptions import *
-from Enterprise import *
-from Logging_ import *
-from RawData import *
-from ConditionalRequestUpdate import *
-from Persistence import *
-from ExposeAllAttributes import *
-from BadAttributes import *
-from Equality import *
+import Framework
 
-from Issue33 import *
-from Issue50 import *
-from Issue54 import *
-from Issue80 import *
-from Issue87 import *
-from Issue131 import *
-from Issue133 import *
-from Issue134 import *
-from Issue139 import *
-from Issue140 import *
-from Issue142 import *
-from Issue158 import *
-from Issue174 import *
-from Issue214 import *
+
+class Issue214(Framework.TestCase):  # https://github.com/jacquev6/PyGithub/issues/214
+    def setUp(self):
+        Framework.TestCase.setUp(self)
+        self.repo = self.g.get_user().get_repo("PyGithub")
+        self.issue = self.repo.get_issue(1)
+
+    def testAssignees(self):
+        self.assertTrue(self.repo.has_in_assignees('farrd'))
+        self.assertFalse(self.repo.has_in_assignees('fake'))
+
+    def testCollaborators(self):
+        self.assertTrue(self.repo.has_in_collaborators('farrd'))
+        self.assertFalse(self.repo.has_in_collaborators('fake'))
+
+        self.assertFalse(self.repo.has_in_collaborators('marcmenges'))
+        self.repo.add_to_collaborators('marcmenges')
+        self.assertTrue(self.repo.has_in_collaborators('marcmenges'))
+
+        self.repo.remove_from_collaborators('marcmenges')
+        self.assertFalse(self.repo.has_in_collaborators('marcmenges'))
+
+    def testEditIssue(self):
+        self.assertEqual(self.issue.assignee, None)
+
+        self.issue.edit(assignee='farrd')
+        self.assertEqual(self.issue.assignee.login, 'farrd')
+
+        self.issue.edit(assignee=None)
+        self.assertEqual(self.issue.assignee, None)
+
+    def testCreateIssue(self):
+        issue = self.repo.create_issue("Issue created by PyGithub", assignee='farrd')
+        self.assertEqual(issue.assignee.login, 'farrd')
+
+    def testGetIssues(self):
+        issues = self.repo.get_issues(assignee='farrd')
+
+        for issue in issues:
+            self.assertEqual(issue.assignee.login, 'farrd')
+
