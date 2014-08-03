b9a67cdef192797ecd9b3ccc62261eb515398321
Vincent Jacques <vincent@vincent-jacques.net>
2014-08-03 16:33:33 -0700

    Restore test coverage for Issue

--- manage/setup_enterprise_test_resources.py
+++ manage/setup_enterprise_test_resources.py
@@ -22,14 +22,17 @@ def verify(o, **attrs):
 
 
 b = PyGithub.BlockingBuilder().Enterprise("github.home.jacquev6.net")
+gZeus = b.Login("zeus", "password1-zeus").Build()
+gPoseidon = b.Login("poseidon", "password1-poseidon").Build()
+gAntigone = b.Login("antigone", "password1-antigone").Build()
+gElectra = b.Login("electra", "password1-electra").Build()
+gPenelope = b.Login("penelope", "password1-penelope").Build()
 
 # There is no API to create users, so we need to create them manually
-gZeus = b.Login("zeus", "password1-zeus").Build()
 zeus = gZeus.get_authenticated_user()
 verify(zeus, site_admin=True, suspended_at=None)
 ensure(zeus, name="Zeus, god of the sky", email="ghe-zeus@jacquev6.net", location="High in the sky", blog="http://jacquev6.net/zeus", hireable=False, company="Zeus Software")
 
-gPoseidon = b.Login("poseidon", "password1-poseidon").Build()
 poseidon = gPoseidon.get_authenticated_user()
 verify(poseidon, site_admin=True, suspended_at=None)
 ensure(poseidon, name="Poseidon, god of the sea", email="ghe-poseidon@jacquev6.net", location="Deep in the sea", blog="http://jacquev6.net/poseidon", hireable=False, company="Poseidon Software")
@@ -37,17 +40,16 @@ ensure(poseidon, name="Poseidon, god of the sea", email="ghe-poseidon@jacquev6.n
 assert b.Build().get_user("morpheus").suspended_at
 
 # Do not modify: antigone.updated_at is tested
-gAntigone = b.Login("antigone", "password1-antigone").Build()
 antigone = gAntigone.get_authenticated_user()
 verify(antigone, site_admin=False, suspended_at=None)
 ensure(antigone, name="Antigone", email="ghe-antigone@jacquev6.net", location="Greece", blog="http://jacquev6.net/antigone", hireable=False, company="Antigone Software")
 
-electra = b.Login("electra", "password1-electra").Build().get_authenticated_user()
+electra = gElectra.get_authenticated_user()
 verify(electra, site_admin=False, suspended_at=None)
 ensure(electra, name="Electra", email="ghe-electra@jacquev6.net", location="Greece", blog="http://jacquev6.net/electra", hireable=False, company="Electra Software")
 
 # Modify freely: no test assert anything about this user
-penelope = b.Login("penelope", "password1-penelope").Build().get_authenticated_user()
+penelope = gPenelope.get_authenticated_user()
 verify(penelope, site_admin=False, suspended_at=None)
 ensure(penelope, name=None, email=None, location=None, blog=None, hireable=False, company=None)
 
@@ -67,7 +69,7 @@ for t in olympus.get_teams():
 gods = olympus.create_team("Gods", permission="admin")
 gods.add_to_members("poseidon")
 gods.add_to_members("zeus")
-humans = olympus.create_team("Humans", permission="admin")
+humans = olympus.create_team("Humans", permission="push")
 humans.add_to_members("antigone")
 humans.add_to_members("electra")
 gZeus.get_org("olympus").add_to_public_members("zeus")
@@ -78,3 +80,20 @@ gAntigone.get_org("olympus").add_to_public_members("antigone")
 electra.add_to_following("zeus")
 electra.add_to_following("poseidon")
 poseidon.add_to_following("zeus")
+
+
+# gElectra.get_repo(("electra", "issues")).delete()
+try:
+    gElectra.get_repo(("electra", "issues"))
+except PyGithub.Blocking.ObjectNotFoundException:
+    time.sleep(5)
+    gElectra.get_authenticated_user().create_repo("issues", auto_init=True)
+    time.sleep(5)
+    gElectra.get_repo(("electra", "issues")).create_milestone("Immutable milestone")
+    gElectra.get_repo(("electra", "issues")).create_milestone("Closed milestone", state="closed")
+    gElectra.get_repo(("electra", "issues")).create_milestone("Mutable milestone")
+    gPenelope.get_repo(("electra", "issues")).create_issue("Immutable issue")
+    gPenelope.get_repo(("electra", "issues")).create_issue("Closed issue")
+    gPenelope.get_repo(("electra", "issues")).create_issue("Mutable issue")
+    gElectra.get_repo(("electra", "issues")).get_issue(1).edit(assignee="electra", labels=["enhancement", "question"])
+    gElectra.get_repo(("electra", "issues")).get_issue(2).edit(assignee="electra", labels=["enhancement", "question"], state="closed", milestone=1)
