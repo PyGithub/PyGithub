7a0fcb27b7cd6c346fc3f76216ccb6e0f4ca3bcc
Aaron Levine <allevin@sandia.gov>
2017-03-21 13:00:11 -0600

    Add testing for Pull Request Reviews and Review Requests

--- github/tests/PullRequestReview.py
+++ github/tests/PullRequestReview.py
@@ -4,7 +4,6 @@
 #                                                                              #
 # Copyright 2012 Vincent Jacques <vincent@vincent-jacques.net>                 #
 # Copyright 2012 Zearin <zearin@gonk.net>                                      #
-# Copyright 2013 AKFish <akfish@gmail.com>                                     #
 # Copyright 2013 Vincent Jacques <vincent@vincent-jacques.net>                 #
 #                                                                              #
 # This file is part of PyGithub.                                               #
@@ -25,75 +24,31 @@
 #                                                                              #
 # ##############################################################################
 
-from AuthenticatedUser import *
-from Authentication import *
-from Authorization import *
-from Branch import *
-from Commit import *
-from CommitCombinedStatus import *
-from CommitComment import *
-from CommitStatus import *
-from ContentFile import *
-from Download import *
-from Event import *
-from Gist import *
-from GistComment import *
-from GitBlob import *
-from GitCommit import *
-from Github_ import *
-from GitRef import *
-from GitRelease import *
-from GitTag import *
-from GitTree import *
-from Hook import *
-from Issue import *
-from IssueComment import *
-from IssueEvent import *
-from Label import *
-from Milestone import *
-from NamedUser import *
-from Markdown import *
-from OrganizationHasInMembers import *
-from Organization import *
-from PullRequest import *
-from PullRequestComment import *
-from PullRequestReview import *
-from PullRequestReviewRequests import *
-from PullRequestFile import *
-from RateLimiting import *
-from Repository import *
-from RepositoryKey import *
-from Status import *
-from Tag import *
-from Team import *
-from UserKey import *
+import Framework
 
-from PaginatedList import *
-from Exceptions import *
-from Enterprise import *
-from Logging_ import *
-from RawData import *
-from ConditionalRequestUpdate import *
-from Persistence import *
-from ExposeAllAttributes import *
-from BadAttributes import *
-from Equality import *
-from Search import *
+import datetime
 
-from Issue33 import *
-from Issue50 import *
-from Issue54 import *
-from Issue80 import *
-from Issue87 import *
-from Issue131 import *
-from Issue133 import *
-from Issue134 import *
-from Issue139 import *
-from Issue140 import *
-from Issue142 import *
-from Issue158 import *
-from Issue174 import *
-from Issue214 import *
-from Issue216 import *
-from Issue278 import *
-from Issue494 import *
+
+class PullRequestReview(Framework.TestCase):
+    def setUp(self):
+        Framework.TestCase.setUp(self)
+#        self.pullreview = self.g.get_user().get_repo("PyGithub").get_pull(538).get_review(1):
+        self.pullreview = self.g.get_user().get_repo("PyGithub").get_pull(538).get_reviews(self)
+        for ireview in self.pullreview.get_reviews():
+            iuser     = xstr(ireview.user.login) + " (" + xstr(ireview.user.name) + ")"
+            istate    = xstr(ireview.state)
+            print("* Review #{0} was {1} by User: {2}; For Commit {3}: Comment = {4}".format(ireview.id, istate, iuser, ireview.commit_id, ireview.body))
+            print("*   HTML_URL = {0}".format(ireview.html_url))
+            print("*   Pull Request URL = {0}".format(ireview.pull_request_url))
+
+    def testAttributes(self):
+        self.assertEqual(self.pullreview.body, "Comment created by PyGithub")
+        self.assertEqual(self.pullreview.commit_id, "8a4f306d4b223682dd19410d4a9150636ebe4206")
+        self.assertEqual(self.pullreview.id, 886298)
+        self.assertEqual(self.pullreview.state, "src/github/Issue.py")
+        self.assertEqual(self.pullreview.user.login, "jacquev6")
+        self.assertEqual(self.pullreview.html_url, "https://github.com/jacquev6/PyGithub/pull/170#issuecomment-18637907")
+        self.assertEqual(self.pullreview.pull_request_url, "https://github.com/jacquev6/PyGithub/pull/170#issuecomment-18637907")
+
+        # test __repr__() based on this attributes
+        self.assertEqual(self.comment.__repr__(), 'PullRequestReview(id=886298, user=NamedUser(login="jacquev6"))')
--- github/tests/PullRequestReviewRequests.py
+++ github/tests/PullRequestReviewRequests.py
@@ -4,7 +4,6 @@
 #                                                                              #
 # Copyright 2012 Vincent Jacques <vincent@vincent-jacques.net>                 #
 # Copyright 2012 Zearin <zearin@gonk.net>                                      #
-# Copyright 2013 AKFish <akfish@gmail.com>                                     #
 # Copyright 2013 Vincent Jacques <vincent@vincent-jacques.net>                 #
 #                                                                              #
 # This file is part of PyGithub.                                               #
@@ -25,75 +24,21 @@
 #                                                                              #
 # ##############################################################################
 
-from AuthenticatedUser import *
-from Authentication import *
-from Authorization import *
-from Branch import *
-from Commit import *
-from CommitCombinedStatus import *
-from CommitComment import *
-from CommitStatus import *
-from ContentFile import *
-from Download import *
-from Event import *
-from Gist import *
-from GistComment import *
-from GitBlob import *
-from GitCommit import *
-from Github_ import *
-from GitRef import *
-from GitRelease import *
-from GitTag import *
-from GitTree import *
-from Hook import *
-from Issue import *
-from IssueComment import *
-from IssueEvent import *
-from Label import *
-from Milestone import *
-from NamedUser import *
-from Markdown import *
-from OrganizationHasInMembers import *
-from Organization import *
-from PullRequest import *
-from PullRequestComment import *
-from PullRequestReview import *
-from PullRequestReviewRequests import *
-from PullRequestFile import *
-from RateLimiting import *
-from Repository import *
-from RepositoryKey import *
-from Status import *
-from Tag import *
-from Team import *
-from UserKey import *
+import Framework
 
-from PaginatedList import *
-from Exceptions import *
-from Enterprise import *
-from Logging_ import *
-from RawData import *
-from ConditionalRequestUpdate import *
-from Persistence import *
-from ExposeAllAttributes import *
-from BadAttributes import *
-from Equality import *
-from Search import *
+import datetime
 
-from Issue33 import *
-from Issue50 import *
-from Issue54 import *
-from Issue80 import *
-from Issue87 import *
-from Issue131 import *
-from Issue133 import *
-from Issue134 import *
-from Issue139 import *
-from Issue140 import *
-from Issue142 import *
-from Issue158 import *
-from Issue174 import *
-from Issue214 import *
-from Issue216 import *
-from Issue278 import *
-from Issue494 import *
+
+class PullRequestReviewRequests(Framework.TestCase):
+    def setUp(self):
+        Framework.TestCase.setUp(self)
+        self.pullreviewrequest = self.g.get_user().get_repo("PyGithub").get_pull(538).get_review_requests()
+        print("* Inspection #{0}, Requested from User {1}".format(self.pullreviewrequest.id, self.pullreviewrequest.login))
+
+    def testAttributes(self):
+        self.assertEqual(self.pullreviewrequest.id, 886298)
+        self.assertEqual(self.pullreviewrequest.login, "jacquev6")
+
+        # test __repr__() based on this attributes
+        self.assertEqual(self.comment.__repr__(), 'PullRequestReviewRequest(id=886298, user="jacquev6"')
+
