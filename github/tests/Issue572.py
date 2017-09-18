4ef489c7e622730163cb8ce2fdd1387de1bb701b
tmshn <tmshn@r.recruit.co.jp>
2017-09-18 23:20:42 +0900

    Added Issue.as_pull_request() & PullReqest.as_issue()

--- github/tests/Issue572.py
+++ github/tests/Issue572.py
@@ -4,7 +4,7 @@
 #                                                                              #
 # Copyright 2012 Vincent Jacques <vincent@vincent-jacques.net>                 #
 # Copyright 2012 Zearin <zearin@gonk.net>                                      #
-# Copyright 2013 AKFish <akfish@gmail.com>                                     #
+# Copyright 2013 Stuart Glaser <stuglaser@gmail.com>                           #
 # Copyright 2013 Vincent Jacques <vincent@vincent-jacques.net>                 #
 #                                                                              #
 # This file is part of PyGithub.                                               #
@@ -25,76 +25,23 @@
 #                                                                              #
 # ##############################################################################
 
-from AuthenticatedUser import *
-from Authentication import *
-from Authorization import *
-from Branch import *
-from Commit import *
-from CommitCombinedStatus import *
-from CommitComment import *
-from CommitStatus import *
-from ContentFile import *
-from Download import *
-from Event import *
-from Gist import *
-from GistComment import *
-from GitBlob import *
-from GitCommit import *
-from Github_ import *
-from GitRef import *
-from GitRelease import *
-from GitTag import *
-from GitTree import *
-from Hook import *
-from Issue import *
-from IssueComment import *
-from IssueEvent import *
-from Label import *
-from Milestone import *
-from NamedUser import *
-from Markdown import *
-from OrganizationHasInMembers import *
-from Organization import *
-from PullRequest import *
-from PullRequestComment import *
-from PullRequestReview import *
-from PullRequestReviewerRequests import *
-from PullRequestFile import *
-from RateLimiting import *
-from Repository import *
-from RepositoryKey import *
-from Status import *
-from Tag import *
-from Team import *
-from UserKey import *
+import Framework
 
-from PaginatedList import *
-from Exceptions import *
-from Enterprise import *
-from Logging_ import *
-from RawData import *
-from ConditionalRequestUpdate import *
-from Persistence import *
-from ExposeAllAttributes import *
-from BadAttributes import *
-from Equality import *
-from Search import *
+import github
 
-from Issue33 import *
-from Issue50 import *
-from Issue54 import *
-from Issue80 import *
-from Issue87 import *
-from Issue131 import *
-from Issue133 import *
-from Issue134 import *
-from Issue139 import *
-from Issue140 import *
-from Issue142 import *
-from Issue158 import *
-from Issue174 import *
-from Issue214 import *
-from Issue216 import *
-from Issue278 import *
-from Issue494 import *
-from Issue572 import *
+class Issue572(Framework.TestCase):
+    def setUp(self):
+        Framework.TestCase.setUp(self)
+        self.repo = self.g.get_user().get_repo("PyGithub")
+
+    def testIssueAsPullRequest(self):
+        issue = self.repo.get_issue(2)
+        pull = issue.as_pull_request()
+        self.assertEqual(issue.html_url, pull.html_url)
+        self.assertTrue(isinstance(pull, github.PullRequest.PullRequest))
+
+    def testPullReqeustAsIssue(self):
+        pull = self.repo.get_pull(2)
+        issue = pull.as_issue()
+        self.assertEqual(pull.html_url, issue.html_url)
+        self.assertTrue(isinstance(issue, github.Issue.Issue))
