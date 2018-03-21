fb9322abc238ad210e88dc32a68979f8a67fec4f
Wan Liuyang <tsfdye@gmail.com>
2018-03-21 14:01:05 +0800

    Update SourceImport.py

--- github/tests/SourceImport.py
+++ github/tests/SourceImport.py
@@ -7,7 +7,7 @@
 # Copyright 2013 Vincent Jacques <vincent@vincent-jacques.net>                 #
 #                                                                              #
 # This file is part of PyGithub.                                               #
-# http://pygithub.github.io/PyGithub/v1/index.html                             #
+# http://pygithub.readthedocs.io/                                              #
 #                                                                              #
 # PyGithub is free software: you can redistribute it and/or modify it under    #
 # the terms of the GNU Lesser General Public License as published by the Free  #
