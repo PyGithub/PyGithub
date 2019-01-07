a7ca0aa4b0e54c0cdfb5d79c2b32eb894e55135c
surya teja reddy valluri <suryav@juniper.net>
2019-01-07 06:53:33 -0800

    successful tests

--- github/tests/AuthenticatedUser.py
+++ github/tests/AuthenticatedUser.py
@@ -72,6 +72,7 @@ class AuthenticatedUser(Framework.TestCase):
         self.assertEqual(self.user.total_private_repos, 5)
         self.assertEqual(self.user.type, "User")
         self.assertEqual(self.user.url, "https://api.github.com/users/jacquev6")
+        self.assertEqual(self.user.node_id, "MDQ6VXNlcjMyNzE0Ng==")
 
         # test __repr__() based on this attributes
         self.assertEqual(self.user.__repr__(), 'AuthenticatedUser(login="jacquev6")')
