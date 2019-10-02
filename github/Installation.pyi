912b938e77765995a54399553881bfc1a838de40
AGirard <bouhahah@gmail.com>
2019-10-02 16:00:51 +0200

    fix Installation

--- github/Installation.pyi
+++ github/Installation.pyi
@@ -1,4 +1,5 @@
 from github.GithubObject import _ValuedAttribute
+from github.PaginatedList import PaginatedList
 from typing import (
     Any,
     Dict,
@@ -10,3 +11,4 @@ class Installation:
     def _useAttributes(self, attributes: Dict[str, Any]) -> None: ...
     @property
     def id(self) -> _ValuedAttribute: ...
+    def get_repos(self) -> PaginatedList: ...
