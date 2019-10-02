eede731dfeff53fb8b6d2fa6ee8bd05cd3d2fa2d
AGirard <bouhahah@gmail.com>
2019-10-02 15:58:42 +0200

    fix InputGitAuthor

--- github/InputGitAuthor.pyi
+++ github/InputGitAuthor.pyi
@@ -1,7 +1,8 @@
-from typing import Dict
+from typing import Dict, Union
+from github.GithubObject.NotSet import _NotSetType
 
 
 class InputGitAuthor:
-    def __init__(self, name: str, email: str, date: str = ...) -> None: ...
+    def __init__(self, name: str, email: str, date: Union[str, _NotSetType] = ...) -> None: ...
     @property
     def _identity(self) -> Dict[str, str]: ...
