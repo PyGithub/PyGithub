835ff12a276a8bc6153cc56e327742e825887263
Adam Baratz <adam.baratz@gmail.com>
2019-10-10 11:06:56 -0400

    Add test coverage for Topic properties

--- tests/Topic.py
+++ tests/Topic.py
@@ -24,13 +24,35 @@
 
 from __future__ import absolute_import
 
+from datetime import datetime
 from operator import attrgetter
 
 from . import Framework
 
 
 class Topic(Framework.TestCase):
-    def testSearch(self):
+    def setUp(self):
+        Framework.TestCase.setUp(self)
+        self.topics = list(self.g.search_topics("python"))
+
+    def testAllFields(self):
+        topic = self.topics[0]
+
+        self.assertEqual(topic.name, "python")
+        self.assertEqual(topic.display_name, "Python")
+        self.assertEqual(topic.short_description, "Python is a dynamically typed programming language.")
+        self.assertEqual(topic.description, "Python is a dynamically typed programming language designed by Guido van Rossum. Much like the programming language Ruby, Python was designed to be easily read by programmers. Because of its large following and many libraries, Python can be implemented and used to do anything from webpages to scientific research.")
+        self.assertEqual(topic.created_by, "Guido van Rossum")
+        self.assertEqual(topic.released, "February 20, 1991")
+        self.assertEqual(topic.created_at, datetime(2016, 12, 7, 0, 7, 2))
+        self.assertEqual(topic.updated_at, datetime(2019, 10, 9, 20, 33, 49))
+        self.assertEqual(topic.featured, True)
+        self.assertEqual(topic.curated, True)
+        self.assertEqual(topic.score, 7576.306)
+
+        self.assertEqual(topic.__repr__(), u'Topic(name="python")')
+
+    def testNamesFromSearchResults(self):
         expected_names = [
             'python',
             'django',
@@ -64,4 +86,4 @@ class Topic(Framework.TestCase):
             'python-2-7',
         ]
 
-        self.assertListKeyEqual(self.g.search_topics("python"), attrgetter("name"), expected_names)
+        self.assertListKeyEqual(self.topics, attrgetter("name"), expected_names)
