b797cac02194e9651390e71139242de6f285c7e1
Wan Liuyang <tsfdye@gmail.com>
2019-12-10 23:19:55 +0800

    Fix rtd build

--- doc/conf.py
+++ doc/conf.py
@@ -33,12 +33,11 @@ import glob
 import os
 import sys
 
-from setup import version as setupVersion  # noqa: E402
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
+from setup import version as setupVersion  # noqa: E402, isort:skip
 
 # -- General configuration -----------------------------------------------------
 
