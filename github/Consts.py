84bb69abffb5eeb934a7fc51aef87c9018dc7561
Steve Kowalik <steven@wedontsleep.org>
2020-04-26 12:05:20 +1000

    Drop use of shadow-cat for draft PRs (#1469)

--- github/Consts.py
+++ github/Consts.py
@@ -118,8 +118,5 @@ updateBranchPreview = "application/vnd.github.lydian-preview+json"
 # https://developer.github.com/changes/2016-05-23-timeline-preview-api/
 issueTimelineEventsPreview = "application/vnd.github.mockingbird-preview"
 
-# https://developer.github.com/changes/2019-02-14-draft-pull-requests/
-draftPullRequestPreview = "application/vnd.github.shadow-cat-preview+json"
-
 # https://developer.github.com/v3/teams/#check-if-a-team-manages-a-repository
 teamRepositoryPermissions = "application/vnd.github.v3.repository+json"
