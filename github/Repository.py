69520d61b29e9b0be5b12fd4c0f65117c4a9c8ce
Ruben Scheja <ruben.scheja@sap.com>
2020-05-28 00:54:39 +0200

    add change_git_ref method in Repository

--- github/Repository.py
+++ github/Repository.py
@@ -966,6 +966,25 @@ class Repository(github.GithubObject.CompletableGithubObject):
         )
         return github.GitRef.GitRef(self._requester, headers, data, completed=True)
 
+    def change_git_ref(self, ref, sha, force=False):
+        """
+        :calls: `PATCH /repos/:owner/:repo/git/refs/:ref <http://developer.github.com/v3/git/refs>`_
+        :param ref: string
+        :param sha: string
+        :param force: bool
+        :rtype: :class:`github.GitRef.GitRef`
+        """
+        assert isinstance(ref, str), ref
+        assert isinstance(sha, str), sha
+        patch_parameters = {
+            "sha": sha,
+            "force": force
+        }
+        headers, data = self._requester.requestJsonAndCheck(
+            "PATCH", self.url + "/git/refs/" + ref, input=patch_parameters
+        )
+        return github.GitRef.GitRef(self._requester, headers, data, completed=True)
+        
     def create_git_tag_and_release(
         self,
         tag,
