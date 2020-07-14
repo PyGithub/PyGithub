4d5560b03c9ef81b8ab34fb72ece84af7d41ca47
Jesse Li <jesse.li2002@gmail.com>
2020-07-13 20:02:48 -0700

    Reorganized existing release tests

--- tests/GitRelease.py
+++ tests/GitRelease.py
@@ -39,12 +39,104 @@ import os
 import zipfile
 
 from . import Framework
+from github import GithubException
+
+repo_name = "RepoTest"
+user = "rickrickston123"
+release_id = 28524234
+author_id = 64711998
+tag = "v1.0"
+create_date = datetime.datetime(2020, 7, 12, 7, 34, 42)
+publish_date = datetime.datetime(2020, 7, 14, 0, 58, 20)
 
 
-class Release(Framework.TestCase):
+class ReleaseRead(Framework.TestCase):
+    """
+    Tests that only involve reading, not modification. All data comes from an existing release manually crafted.
+    """
+
+    # Note that it's not possible to specify `self.release` in setUp() because something goes wonky when using --record
+    def testAttributes(self):
+        release = self.g.get_user(user).get_repo(repo_name).get_latest_release()
+        self.assertEqual(release.id, release_id)
+        self.assertEqual(release.tag_name, tag)
+        self.assertEqual(release.target_commitish, "master")
+        self.assertEqual(
+            release.upload_url,
+            "https://uploads.github.com/repos/{}/{}/releases/{}/assets{{?name,label}}".format(
+                user, repo_name, release_id
+            ),
+        )
+        self.assertEqual(release.body, "Body")
+        self.assertEqual(release.title, "Test")
+        self.assertEqual(release.draft, False)
+        self.assertEqual(release.prerelease, False)
+        self.assertEqual(
+            release.url,
+            "https://api.github.com/repos/{}/{}/releases/{}".format(
+                user, repo_name, release_id
+            ),
+        )
+        self.assertEqual(release.author._rawData["login"], user)
+        self.assertEqual(release.author.login, user)
+        self.assertEqual(release.author.id, author_id)
+        self.assertEqual(release.author.type, "User")
+        self.assertEqual(
+            release.html_url,
+            "https://github.com/{}/{}/releases/tag/{}".format(user, repo_name, tag),
+        )
+        self.assertEqual(release.created_at, create_date)
+        self.assertEqual(release.published_at, publish_date)
+        self.assertEqual(
+            release.tarball_url,
+            "https://api.github.com/repos/{}/{}/tarball/{}".format(
+                user, repo_name, tag
+            ),
+        )
+        self.assertEqual(
+            release.zipball_url,
+            "https://api.github.com/repos/{}/{}/zipball/{}".format(
+                user, repo_name, tag
+            ),
+        )
+        self.assertEqual(repr(release), 'GitRelease(title="Test")')
+
+    def testGetRelease(self):
+        release_by_id = (
+            self.g.get_user(user).get_repo(repo_name).get_release(release_id)
+        )
+        release_by_tag = self.g.get_user(user).get_repo(repo_name).get_release(tag)
+        self.assertEqual(release_by_id, release_by_tag)
+
+    def testGetLatestRelease(self):
+        repo = self.g.get_user(user).get_repo(repo_name)
+        latest_release = repo.get_latest_release()
+        self.assertEqual(latest_release.tag_name, tag)
+
+    def testGetAssets(self):
+        """
+        Test retrieving the set of assets for the current release, as well as directly by id.
+        """
+        repo = self.g.get_user(user).get_repo(repo_name)
+        release = repo.get_release(release_id)
+        self.assertEqual(release.id, release_id)
+
+        asset_list = [x for x in release.get_assets()]
+        self.assertTrue(asset_list is not None)
+        self.assertEqual(len(asset_list), 1)
+
+        asset_id = asset_list[0].id
+        asset = repo.get_release_asset(asset_id)
+        self.assertTrue(asset is not None)
+        self.assertEqual(asset.id, asset_id)
+
+
+class ReleaseModify(Framework.TestCase):
+    """ Tests involving creating/updating/deleting releases."""
+
     def setUp(self):
         super().setUp()
-        # Do not get self.release here as it casues bad data to be saved in --record mode
+        self.new_tag = "v1.25.2"  # Used for new releases
         self.content_path = "content.txt"
         self.artifact_path = "archive.zip"
 
@@ -60,126 +152,83 @@ class Release(Framework.TestCase):
             os.remove(self.content_path)
         if os.path.exists(self.artifact_path):
             os.remove(self.artifact_path)
+
         super().tearDown()
 
-    def testAttributes(self):
-        self.release = self.g.get_user().get_repo("PyGithub").get_releases()[0]
-        self.assertEqual(self.release.id, 1210814)
-        self.assertEqual(self.release.tag_name, "v1.25.2")
-        self.assertEqual(self.release.target_commitish, "master")
-        self.assertEqual(
-            self.release.upload_url,
-            "https://uploads.github.com/repos/edhollandAL/PyGithub/releases/1210814/assets{?name}",
-        )
-        self.assertEqual(self.release.body, "Body")
-        self.assertEqual(self.release.title, "Test")
-        self.assertEqual(self.release.draft, False)
-        self.assertEqual(self.release.prerelease, False)
-        self.assertEqual(
-            self.release.url,
-            "https://api.github.com/repos/edhollandAL/PyGithub/releases/1210814",
-        )
-        self.assertEqual(self.release.author._rawData["login"], "edhollandAL")
-        self.assertEqual(self.release.author.login, "edhollandAL")
-        self.assertEqual(self.release.author.id, 11922660)
-        self.assertEqual(self.release.author.type, "User")
-        self.assertEqual(
-            self.release.html_url,
-            "https://github.com/edhollandAL/PyGithub/releases/tag/v1.25.2",
-        )
-        self.assertEqual(self.release.created_at, datetime.datetime(2014, 10, 8, 1, 54))
-        self.assertEqual(
-            self.release.published_at, datetime.datetime(2015, 4, 24, 8, 36, 51)
-        )
-        self.assertEqual(
-            self.release.tarball_url,
-            "https://api.github.com/repos/edhollandAL/PyGithub/tarball/v1.25.2",
-        )
-        self.assertEqual(
-            self.release.zipball_url,
-            "https://api.github.com/repos/edhollandAL/PyGithub/zipball/v1.25.2",
-        )
-        self.assertEqual(repr(self.release), 'GitRelease(title="Test")')
-
-    def testDelete(self):
-        self.release = self.g.get_user().get_repo("PyGithub").get_releases()[0]
-        self.release.delete_release()
-
-    def testUpdate(self):
-        self.release = self.g.get_user().get_repo("PyGithub").get_releases()[0]
-        new_release = self.release.update_release("Updated Test", "Updated Body")
-        self.assertEqual(new_release.body, "Updated Body")
-        self.assertEqual(new_release.title, "Updated Test")
-
-    def testGetRelease(self):
-        release_by_id = self.g.get_user().get_repo("PyGithub").get_release("v1.25.2")
-        release_by_tag = self.g.get_user().get_repo("PyGithub").get_release(1210837)
-        self.assertEqual(release_by_id, release_by_tag)
-
-    def testCreateGitTagAndRelease(self):
-        self.repo = self.g.get_user().get_repo("PyGithub")
-        self.release = self.repo.create_git_tag_and_release(
-            "v3.0.0",
+    # At time of writing (2020-07) the --record option only records setUp() once for the entire class. This causes issues here because for each different test you'll want to create a new release, but the release ID will change between calls, leading to --record collecting bad data.
+    # The workaround I've come up with is to have a seperate setup/teardown method that gets manually called in each test method.
+    def setUpEach(self):
+        self.repo = self.g.get_user(user).get_repo(repo_name)
+        repo = self.repo
+        commit_sha = repo.get_commits()[0].sha  # Just need any commit
+        new_release = repo.create_git_tag_and_release(
+            self.new_tag,
             "tag message",
             "release title",
             "release message",
-            "5a05a5e58f682d315acd2447c87ac5b4d4fc55e8",
+            commit_sha,
             "commit",
         )
-        self.assertEqual(self.release.tag_name, "v3.0.0")
-        self.assertEqual(self.release.body, "release message")
-        self.assertEqual(self.release.title, "release title")
-        self.assertEqual(self.release.author._rawData["login"], "edhollandAL")
-        self.assertEqual(
-            self.release.html_url,
-            "https://github.com/edhollandAL/PyGithub/releases/tag/v3.0.0",
-        )
+        self.new_release_id = new_release.id
+        # assert self.new_release_id != release_id
 
-    def testGetLatestRelease(self):
-        self.repo = self.g.get_user().get_repo("PyGithub")
-        latest_release = self.repo.get_latest_release()
-        self.assertEqual(latest_release.tag_name, "v1.25.2")
+    def tearDownEach(self):
+        try:
+            new_release = self.repo.get_release(self.new_release_id)
+            new_release.delete_release()
+        except GithubException:
+            pass  # Already deleted
 
-    def testGetAsset(self):
-        """
-        Test retrieving a release asset directly by its ID.
-        """
-        the_repo = self.g.get_user().get_repo("PyGithub")
+    def testDelete(self):
+        # repo = self.g.get_user(user).get_repo(repo_name)
+        self.setUpEach()
+        to_delete = self.repo.get_release(self.new_release_id)
+        to_delete.delete_release()
+        self.tearDownEach()
 
-        asset_id = 16
-        the_asset = the_repo.get_release_asset(asset_id)
-        self.assertTrue(the_asset is not None)
-        self.assertEqual(the_asset.id, asset_id)
-
-    def testGetAssets(self):
-        """
-        Test retrieving the set of assets for the current release.
-        """
-        release_id = 1210837
-        the_repo = self.g.get_user().get_repo("PyGithub")
-        the_release = the_repo.get_release(release_id)
-        self.assertEqual(the_release.id, release_id)
-
-        asset_list = [x for x in the_release.get_assets()]
-        self.assertTrue(asset_list is not None)
-        self.assertEqual(len(asset_list), 1)
+    def testUpdate(self):
+        self.setUpEach()
+        repo = self.repo
+        release = repo.get_release(self.new_release_id)
+        new_release = release.update_release("Updated Test", "Updated Body")
+        self.assertEqual(new_release.title, "Updated Test")
+        self.assertEqual(new_release.body, "Updated Body")
+        self.tearDownEach()
 
     def testUploadAsset(self):
         """
         Test uploading a new asset to the release.
         """
-        release_id = 1210837
-        the_repo = self.g.get_user().get_repo("PyGithub")
-        the_release = the_repo.get_release(release_id)
-        self.assertEqual(the_release.id, release_id)
+        self.setUpEach()
+        repo = self.repo
+        release = repo.get_release(self.new_release_id)
+        self.assertEqual(release.id, self.new_release_id)
 
-        the_release.upload_asset(
+        release.upload_asset(
             self.artifact_path, "unit test artifact", "application/zip"
         )
+        self.tearDownEach()
 
     def testUploadAssetWithName(self):
-        release_id = 1210837
-        repo = self.g.get_user().get_repo("PyGithub")
-        release = repo.get_release(release_id)
+        self.setUpEach()
+        release = self.repo.get_release(self.new_release_id)
         r = release.upload_asset(self.artifact_path, name="foobar.zip")
         self.assertEqual(r.name, "foobar.zip")
+        self.tearDownEach()
+
+    def testCreateGitTagAndRelease(self):
+        self.setUpEach()
+        # Creation code already done in setup, so we'll just test what's already here.
+        repo = self.repo
+        release = repo.get_release(self.new_release_id)
+        self.assertEqual(release.tag_name, self.new_tag)
+        self.assertEqual(release.body, "release message")
+        self.assertEqual(release.title, "release title")
+        self.assertEqual(release.author._rawData["login"], user)
+        self.assertEqual(
+            release.html_url,
+            "https://github.com/{}/{}/releases/tag/{}".format(
+                user, repo_name, self.new_tag
+            ),
+        )
+        self.tearDownEach()
