fdcbc12ef0efeb23c0777b98eda2682e2423af81
Raju Subramanian <coder@mahesh.net>
2020-08-11 22:48:19 -0700

    Re-build tests/ExportAllAttributes.py
    The change to last_modified for ContentFile has introduced an
    additional API request. This breaks the recorded requests in
    the replay data for this test. The only way to fix this is to
    re-build the testcase and its replaydata. Now the test itself
    is failing because a lot of Classes are out of sync with the
    Github API.

--- tests/ExposeAllAttributes.py
+++ tests/ExposeAllAttributes.py
@@ -32,14 +32,14 @@ class ExposeAllAttributes(Framework.TestCase):
     def testAllClasses(self):
         authenticatedUser = self.g.get_user()
         namedUser = self.g.get_user("nvie")
-        repository = authenticatedUser.get_repo("PyGithub")
-        organization = self.g.get_organization("BeaverSoftware")
+        repository = self.g.get_repo("wrecker/PyGithub")
+        organization = self.g.get_organization("simplecloudsolution")
         plan = authenticatedUser.plan
-        branch = repository.get_branch("master")
+        branch = repository.get_branch("main")
         commit = repository.get_commit("1292bf0e22c796e91cc3d6e24b544aece8c21f2a")
         commitStats = commit.stats
         commitStatus = commit.get_statuses()[0]
-        milestone = repository.get_milestone(17)
+        milestone = repository.get_milestone(1)
         gist = self.g.get_gist("149016")
         gistComment = gist.get_comment(4565)
         gistFile = gist.files[".gitignore"]
@@ -53,27 +53,26 @@ class ExposeAllAttributes(Framework.TestCase):
         gitBlob = repository.get_git_blob("681fb61f1761743a02f5c790f1c762cbfe8cfad1")
         gitRef = repository.get_git_ref("tags/v1.17.0")
         gitObject = gitRef.object
-        issue = repository.get_issue(188)
-        issueComment = issue.get_comment(22686536)
+        issue = repository.get_issue(1)
+        issueComment = issue.get_comment(672573285)
         issueEvent = issue.get_events()[0]
         issuePullRequest = issue.pull_request
         gitignoreTemplate = self.g.get_gitignore_template("Python")
-        team = organization.get_team(141487)
+        team = organization.get_team(3997153)
         label = repository.get_label("Bug")
-        pullRequest = repository.get_pull(31)
-        pullRequestComment = pullRequest.get_review_comment(1580134)
+        pullRequest = repository.get_pull(1)
+        pullRequestComment = pullRequest.get_review_comment(469006394)
         pullRequestPart = pullRequest.base
         file = pullRequest.get_files()[0]
-        commitComment = repository.get_comment(3630301)
+        commitComment = repository.get_comment(41395834)
         rateLimit = self.g.get_rate_limit()
         hook = repository.get_hooks()[0]
         hookResponse = hook.last_response
-        hookDescription = self.g.get_hooks()[0]
-        comparison = repository.compare("master", "develop")
-        contentFile = repository.get_contents("README.rst")
+        comparison = repository.compare("main", "workflow_dispatch")
+        contentFile = repository.get_contents("README.md")
         permissions = repository.permissions
         event = repository.get_events()[0]
-        notification = authenticatedUser.get_notification("8406712")
+        notification = authenticatedUser.get_notification("885161191")
         notificationSubject = notification.subject
 
         missingAttributes = self.gatherMissingAttributes(
@@ -105,7 +104,6 @@ class ExposeAllAttributes(Framework.TestCase):
                 gitTree,
                 gitTreeElement,
                 hook,
-                hookDescription,
                 hookResponse,
                 issue,
                 issueComment,
