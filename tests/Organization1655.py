3560a0e00e6811ad11ac04e7829b19febf2064e9
Alexandre Delisle <alexodelisle@gmail.com>
2020-09-22 20:26:51 -0400

    https://github.com/PyGithub/PyGithub/pull/1676: add missing attribute test

--- tests/Organization1655.py
+++ tests/Organization1655.py
@@ -30,12 +30,12 @@ from . import Framework
 class OrganizationAuthorization(Framework.TestCase):
     subset = {
         "login": "AlexandreODelisle",
-        "credential_id": 5016315,
+        "credential_id": 5077647,
         "credential_type": "personal access token",
-        "token_last_eight": "6045b0d7",
+        "token_last_eight": "4c4c6b2c",
         "scopes": ["repo"],
         "credential_authorized_at": datetime.datetime.strptime(
-            "2020-09-21 00:54:02", "%Y-%m-%d %H:%M:%S"
+            "2020-09-23 00:22:07", "%Y-%m-%d %H:%M:%S"
         ),  # Will need to be updated if test re-ran ( delete )
         "credential_accessed_at": None,  # Will need to be updated if test re-ran ( delete )
     }
@@ -43,20 +43,20 @@ class OrganizationAuthorization(Framework.TestCase):
     def setUp(self):
         super().setUp()
         self.org = self.g.get_organization("Learn-Think-Do")
-        self.credentials_list = self.org.get_credential_authorizations()
 
     def testAttributes(self):
-
+        credentials_list = self.org.get_credential_authorizations()
         filtered_credentials_list = list(
             filter(
                 lambda x: x.login == self.subset["login"]
                 and x.credential_id == self.subset["credential_id"],
-                self.credentials_list,
+                credentials_list,
             )
         )
 
         AuthCred = filtered_credentials_list[0]
         self.assertEqual(AuthCred.login, self.subset["login"])
+        self.assertEqual(AuthCred.credential_type, self.subset["credential_type"])
         self.assertEqual(AuthCred.credential_id, self.subset["credential_id"])
         self.assertEqual(AuthCred.token_last_eight, self.subset["token_last_eight"])
         self.assertEqual(AuthCred.scopes, self.subset["scopes"])
@@ -68,45 +68,32 @@ class OrganizationAuthorization(Framework.TestCase):
         )
 
     def testGetCredentialsAuth(self):
+        credentials_list = self.org.get_credential_authorizations()
         if "credential_authorized_at" in self.subset.keys():
             self.subset.pop(
                 "credential_authorized_at"
             )  # because _rawData return a Str for datetime
 
         self.assertTrue(
-            any(
-                [
-                    self.subset.items() <= x._rawData.items()
-                    for x in self.credentials_list
-                ]
-            )
+            any([self.subset.items() <= x._rawData.items() for x in credentials_list])
         )
 
     def testRemoveCredentialAuth(self):
+        credentials_list = self.org.get_credential_authorizations()
         if "credential_authorized_at" in self.subset.keys():
             self.subset.pop(
                 "credential_authorized_at"
             )  # because _rawData return a Str for datetime
 
         self.assertTrue(
-            any(
-                [
-                    self.subset.items() <= x._rawData.items()
-                    for x in self.credentials_list
-                ]
-            )
+            any([self.subset.items() <= x._rawData.items() for x in credentials_list])
         )
         self.assertTrue(
             self.org.remove_credential_authorization(
                 credential_id=self.subset["credential_id"]
             )
         )
-        self.credentials_list = self.org.get_credential_authorizations()
+        credentials_list = self.org.get_credential_authorizations()
         self.assertFalse(
-            any(
-                [
-                    self.subset.items() <= x._rawData.items()
-                    for x in self.credentials_list
-                ]
-            )
+            any([self.subset.items() <= x._rawData.items() for x in credentials_list])
         )
