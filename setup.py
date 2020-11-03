1bb18ab52e9a5997774c9aed4e1521fe382e5b21
Steve Kowalik <steven@wedontsleep.org>
2020-11-03 16:39:06 +1100

    Add support for Python 3.9 (#1735)
    With Python 3.9 released, we should support it. A later commit will
    remove support for Python 3.5.

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "Topic :: Software Development",
         ],
         python_requires=">=3.5",
