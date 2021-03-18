6cd3512daccc84e9accfe0eda48386fc7d953ec0
sameer1996-cmd <71526124+sameer1996-cmd@users.noreply.github.com>
2021-03-18 13:47:29 +0100

    Update PullRequest.py

--- github/PullRequest.py
+++ github/PullRequest.py
@@ -488,9 +488,9 @@ class PullRequest(github.GithubObject.CompletableGithubObject):
             post_parameters["commit_id"] = commit.sha
         if body is not github.GithubObject.NotSet:
             post_parameters["body"] = body
-        post_parameters["event"] = (
-            "COMMENT" if event == github.GithubObject.NotSet else event
-        )
+        if event is not github.GithubObject.NotSet :   
+            post_parameters["event"] = event
+        
         if comments is github.GithubObject.NotSet:
             post_parameters["comments"] = []
         else:
@@ -501,6 +501,47 @@ class PullRequest(github.GithubObject.CompletableGithubObject):
         return github.PullRequestReview.PullRequestReview(
             self._requester, headers, data, completed=True
         )
+
+    def delete_review(
+        self,
+        commit=github.GithubObject.NotSet,
+        body=github.GithubObject.NotSet,
+        event=github.GithubObject.NotSet,
+        comments=github.GithubObject.NotSet,
+        review_id = github.GithubObject.NotSet,
+    ):
+        """
+        :calls: `DELETE /repos/:owner/:repo/pulls/:number/reviews/review_id <https://developer.github.com/v3/pulls/reviews/review_id>`_
+        :param commit: github.Commit.Commit
+        :param body: string
+        :param event: string
+        :param comments: list
+        :param review_id: string
+        :rtype: :class:`github.PullRequestReview.PullRequestReview`
+        """
+        assert commit is github.GithubObject.NotSet or isinstance(
+            commit, github.Commit.Commit
+        ), commit
+        assert body is github.GithubObject.NotSet or isinstance(body, str), body
+        assert event is github.GithubObject.NotSet or isinstance(event, str), event
+        assert comments is github.GithubObject.NotSet or isinstance(
+            comments, list
+        ), comments
+        post_parameters = dict()
+        if commit is not github.GithubObject.NotSet:
+            post_parameters["commit_id"] = commit.sha
+        if body is not github.GithubObject.NotSet:
+            post_parameters["body"] = body
+        if event is not github.GithubObject.NotSet :   
+            post_parameters["event"] = event
+        if comments is github.GithubObject.NotSet:
+            post_parameters["comments"] = []
+        else:
+            post_parameters["comments"] = comments
+        headers, data = self._requester.requestJsonAndCheck(
+            "DELETE", f"{self.url}/reviews/"+review_id, input=post_parameters
+        )
+
 
     def create_review_request(
         self,
