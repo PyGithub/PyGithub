c7dc131a88647074a37545676fb39f0029d4c869
Austin Sasko <austin.sasko@aexp.com>
2021-03-29 13:35:32 -0400

    Adding improvements from PR discussion

--- github/PullRequest.py
+++ github/PullRequest.py
@@ -764,7 +764,7 @@ class PullRequest(CompletableGithubObject):
 
     def restore_branch(self):
         """
-        :meth:`Repository.create_git_ref`
+        Convenience function that calls :meth:`Repository.create_git_ref`
         :rtype: :class:`github.GitRef.GitRef`
         """
         return self.head.repo.create_git_ref(
@@ -773,7 +773,7 @@ class PullRequest(CompletableGithubObject):
 
     def delete_branch(self, force=False):
         """
-        :meth:`GitRef.delete`
+        Convenience function that calls :meth:`GitRef.delete`
         :rtype: bool
         """
         if not force:
