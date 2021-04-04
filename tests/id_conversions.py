d056c5c845165eea70d250dcf3b36dcedfb76553
KOLANICH <KOLANICH@users.noreply.github.com>
2021-04-04 15:52:32 +0000

    Cosmetic changes in tests/id_conversions.py

--- tests/id_conversions.py
+++ tests/id_conversions.py
@@ -3,6 +3,7 @@ from github.id_conversions import db_id_and_type_to_node_id, node_id_to_db_id_an
 from . import Framework
 from github.id_conversions import db_id_and_type_to_node_id, node_id_to_db_id_and_type
 
+
 class TestIDConversions(Framework.TestCase):
     TEST_GQL_ID = "MDEwOlJlcG9zaXRvcnkzNTQ0NDkw"
     TEST_GQL_TYPE_NAME = "Repository"
@@ -13,11 +14,11 @@ class TestIDConversions(Framework.TestCase):
             db_id_and_type_to_node_id(
                 self.__class__.TEST_DB_ID, self.__class__.TEST_GQL_TYPE_NAME
             ),
-            self.__class__.TEST_GQL_ID
+            self.__class__.TEST_GQL_ID,
         )
 
     def test_node_id_to_db_id_and_type(self):
         self.assertEqual(
             node_id_to_db_id_and_type(self.__class__.TEST_GQL_ID),
-            (self.__class__.TEST_DB_ID, self.__class__.TEST_GQL_TYPE_NAME)
+            (self.__class__.TEST_DB_ID, self.__class__.TEST_GQL_TYPE_NAME),
         )
