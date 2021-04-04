77f0e2972fd2c17da43d638cbb9b7b5bb27531b5
KOLANICH <KOLANICH@users.noreply.github.com>
2021-04-04 15:46:50 +0000

    Cosmetic changes in tests/id_conversions.py

--- tests/id_conversions.py
+++ tests/id_conversions.py
@@ -1,5 +1,6 @@
-from . import Framework
+from github.id_conversions import db_id_and_type_to_node_id, node_id_to_db_id_and_type
 
+from . import Framework
 from github.id_conversions import db_id_and_type_to_node_id, node_id_to_db_id_and_type
 
 class TestIDConversions(Framework.TestCase):
@@ -8,7 +9,15 @@ class TestIDConversions(Framework.TestCase):
     TEST_DB_ID = 3544490
 
     def test_db_id_and_type_to_node_id(self):
-        self.assertEqual(db_id_and_type_to_node_id(self.__class__.TEST_DB_ID, self.__class__.TEST_GQL_TYPE_NAME), self.__class__.TEST_GQL_ID)
+        self.assertEqual(
+            db_id_and_type_to_node_id(
+                self.__class__.TEST_DB_ID, self.__class__.TEST_GQL_TYPE_NAME
+            ),
+            self.__class__.TEST_GQL_ID
+        )
 
     def test_node_id_to_db_id_and_type(self):
-        self.assertEqual(node_id_to_db_id_and_type(self.__class__.TEST_GQL_ID), (self.__class__.TEST_DB_ID, self.__class__.TEST_GQL_TYPE_NAME))
+        self.assertEqual(
+            node_id_to_db_id_and_type(self.__class__.TEST_GQL_ID),
+            (self.__class__.TEST_DB_ID, self.__class__.TEST_GQL_TYPE_NAME)
+        )
