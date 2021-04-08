b767e056a81ac30541b9bd1f76eab5530bd903b5
bagashvilit <bagashvilit@allegheny.edu>
2021-04-08 18:30:11 +0400

    Fix test assertion and generate new replay data

--- tests/PullRequestReview1856.py
+++ tests/PullRequestReview1856.py
@@ -3,15 +3,14 @@ import datetime
 from . import Framework
 
 
-class PullRequestReviewIssue(Framework.TestCase):
+class PullRequestReview1856(Framework.TestCase):
     def setUp(self):
         super().setUp()
         pumpkin_repo = self.g.get_repo("CS481-Team-Pumpkin/PyGithub", lazy=True)
-        self.pumpkin_pull = pumpkin_repo.get_pull(3)
-        self.pullreview = self.pumpkin_pull.get_review(631417478)
+        self.pumpkin_pull = pumpkin_repo.get_pull(4)
+        self.pullreview = self.pumpkin_pull.get_review(631460061)
 
     def testDelete(self):
-        before = self.pumpkin_pull.get_reviews()
         self.pullreview.delete()
-        after = self.pumpkin_pull.get_reviews()
-        self.assertNotEqual(list(before), list(after))
+        reviews = self.pumpkin_pull.get_reviews()
+        self.assertEqual(list(reviews), [])
