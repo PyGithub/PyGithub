d7f1193fa4d950256fa8f100c3029593f7ac3893
Soumil Shekdar <sshekdar@vmware.com>
2021-07-21 15:36:19 -0700

    Fixes: #1671

--- github/AuthenticatedUser.py
+++ github/AuthenticatedUser.py
@@ -973,9 +973,11 @@ class AuthenticatedUser(github.GithubObject.CompletableGithubObject):
 
         params = dict()
         if all is not github.GithubObject.NotSet:
-            params["all"] = all
+            # convert True, False to true, false for api parameters
+            params["all"] = str(all).lower() if isinstance(all, bool) else all
         if participating is not github.GithubObject.NotSet:
-            params["participating"] = participating
+            # convert True, False to true, false for api parameters
+            params["participating"] = str(participating).lower() if isinstance(participating, bool) else participating
         if since is not github.GithubObject.NotSet:
             params["since"] = since.strftime("%Y-%m-%dT%H:%M:%SZ")
         if before is not github.GithubObject.NotSet:
