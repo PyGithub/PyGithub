21326610d47d5447c4a2382b807b9640763625fc
Kostya Leschenko <settlerko@gmail.com>
2022-03-30 01:49:17 +0300

    Mark ':' as safe for urlencoding to fix search functionality
    Fixes https://github.com/PyGithub/PyGithub/issues/1308

--- github/Requester.py
+++ github/Requester.py
@@ -605,7 +605,7 @@ class Requester:
         if len(parameters) == 0:
             return url
         else:
-            return f"{url}?{urllib.parse.urlencode(parameters)}"
+            return f"{url}?{urllib.parse.urlencode(parameters, safe=':')}"
 
     def __createConnection(self):
         kwds = {}
