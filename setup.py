9401c4945bac24b7fdf106cb8ec2e341eb8559e7
Jonathan Piché <jpiche@coveo.com>
2022-06-07 09:57:44 -0400

    DT-4874 // clear pyjwt vulnerability

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
         python_requires=">=3.6",
         install_requires=[
             "deprecated",
-            "pyjwt>=2.0",
+            "pyjwt>=2.4",
             "pynacl>=1.4.0",
             "requests>=2.14.0",
         ],
