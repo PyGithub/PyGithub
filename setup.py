3dd39214fb108e3677fc289bca8389618d5c9c1b
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-10-13 14:39:17 +0200

    Require pyjwt>=2.4.0 to avoid CVE-2022-29217

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
         python_requires=">=3.7",
         install_requires=[
             "deprecated",
-            "pyjwt>=2.0",
+            "pyjwt>=2.4.0",
             "pynacl>=1.4.0",
             "requests>=2.14.0",
         ],
