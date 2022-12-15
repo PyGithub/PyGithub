d019453a431d2fcf867a1b300f818f1342b552d0
Enrico Minack <github@enrico.minack.dev>
2022-12-15 12:42:15 +0100

    Improve typing

--- github/GithubRetry.pyi
+++ github/GithubRetry.pyi
@@ -1,4 +1,5 @@
 from datetime import datetime
+from logging import Logger
 from typing import Any
 
 from urllib3 import Retry, HTTPResponse
@@ -6,6 +7,7 @@ from urllib3 import Retry, HTTPResponse
 
 class GithubRetry:
     secondaryRateWait: int
+    __logger: Logger
     def __init__(self, secondaryRateWait: int, **kwargs: Any) -> None: ...
     def increment(
         self,
@@ -18,5 +20,5 @@ class GithubRetry:
     ) -> Retry: ...
     @staticmethod
     def get_content(resp: HTTPResponse, url: str) -> str: ...
-    def __utc_now(self) -> datetime:
+    def __utc_now(self) -> datetime: ...
     def __log(self, level: int, message, **kwargs) -> None: ...
