9c96faa702c5c050f50d5d9e46eaaed2e02d8a00
Liuyang Wan <tsfdye@gmail.com>
2023-02-19 19:50:45 +0800

    Add current dir sys.path as well

--- doc/conf.py
+++ doc/conf.py
@@ -35,6 +35,7 @@ import sys
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
 from importlib.metadata import version  # noqa: E402, isort:skip
 
 setupVersion = version("pygithub")
