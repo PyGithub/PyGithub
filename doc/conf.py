def5223c215e6c6d8470417dbb2488d4dadfe8ee
Liuyang Wan <tsfdye@gmail.com>
2023-02-20 09:22:59 +0800

    RTD: install current project

--- doc/conf.py
+++ doc/conf.py
@@ -35,7 +35,6 @@ import sys
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("."))
 from importlib.metadata import version  # noqa: E402, isort:skip
 
 setupVersion = version("pygithub")
