99eb1a247c812b3077f1bc6bf61e7c5c94a831e4
Jacob Beard <jake@minnow.io>
2023-03-05 14:04:55 -0700

    Added test for required_linear_history attribute and replay data

---
+++ tests/BranchProtection2.py
@@ -0,0 +1,47 @@
+############################ Copyrights and license ############################
+#                                                                              #
+# Copyright 2018 Steve Kowalik <steven@wedontsleep.org>                        #
+#                                                                              #
+# This file is part of PyGithub.                                               #
+# http://pygithub.readthedocs.io/                                              #
+#                                                                              #
+# PyGithub is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU Lesser General Public License as published by the Free  #
+# Software Foundation, either version 3 of the License, or (at your option)    #
+# any later version.                                                           #
+#                                                                              #
+# PyGithub is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    #
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more #
+# details.                                                                     #
+#                                                                              #
+# You should have received a copy of the GNU Lesser General Public License     #
+# along with PyGithub. If not, see <http://www.gnu.org/licenses/>.             #
+#                                                                              #
+################################################################################
+
+from . import Framework
+
+
+class BranchProtection2(Framework.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.branch_protection = (
+            self.g
+            .get_repo("curvewise-forks/PyGithub")
+            .get_branch("master")
+            .get_protection()
+        )
+
+    def testAttributes(self):
+        self.assertTrue(self.branch_protection.required_linear_history)
+        self.assertEqual(
+            self.branch_protection.url,
+            'https://api.github.com/repos/curvewise-forks/PyGithub/branches/master/protection',
+        )
+        self.assertEqual(
+            self.branch_protection.__repr__(),
+            'BranchProtection(url="https://api.github.com/repos/curvewise-forks/PyGithub/branches/master/protection")',
+        )
+
+        
