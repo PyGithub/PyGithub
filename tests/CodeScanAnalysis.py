cb8508fd1f6211fc901f41d3143c4663b0f66cff
Tristen Bronson <tbronson@coveo.com>
2023-04-20 16:07:13 -0400

    * Added Repository test for security and analysis * Added Repository test for get code scan analyses * Added CodeScanAnalysis attributes test

---
+++ tests/CodeScanAnalysis.py
@@ -0,0 +1,33 @@
+from . import Framework
+
+
+class CodeScanAnalysis(Framework.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.repo = self.g.get_user().get_repo("PyGithub")
+
+    def testAttributes(self):
+        analysis = self.repo.get_code_scanning_analyses()[0]
+
+        self.assertIn("refs/heads/main", analysis.ref)
+        self.assertIn("d99612c3e1f2970085cfbaeadf8f010ef69bad83", analysis.commit_sha)
+        self.assertIn(
+            ".github/workflows/codeql-analysis.yml:analyze", analysis.analysis_key
+        )
+        self.assertIn('{"language":"python"}', analysis.environment)
+        self.assertIn("", analysis.error)
+        self.assertIn(
+            ".github/workflows/codeql-analysis.yml:analyze/language:python",
+            analysis.category,
+        )
+        self.assertIn("2020-08-27T15:05:21Z", analysis.created_at)
+        self.assertEqual(17, analysis.results_count)
+        self.assertEqual(49, analysis.rules_count)
+        self.assertEqual(201, analysis.id)
+        self.assertIn(
+            "https://api.github.com/repos/octocat/hello-world/code-scanning/analyses/201",
+            analysis.url,
+        )
+        self.assertIn("6c81cd8e-b078-4ac3-a3be-1dad7dbd0b53", analysis.sarif_id)
+        self.assertEqual(True, analysis.deletable)
+        self.assertIn("", analysis.warning)
---
+++ tests/Tool.py
@@ -0,0 +1,14 @@
+from . import Framework
+
+
+class Tool(Framework.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.tool = (
+            self.g.get_user().get_repo("PyGithub").get_code_scanning_analyses()[0].tool
+        )
+
+    def testAttributes(self):
+        self.assertIn("CodeQL", self.tool.name)
+        self.assertEqual(None, self.tool.guid)
+        self.assertIn("2.4.0", self.tool.version)
