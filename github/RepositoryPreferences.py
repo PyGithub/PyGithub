efd265f4efd6516a693db6b2455ac28fcb484f73
Trim21 <trim21.me@gmail.com>
2023-06-27 21:39:51 +0800

    Update RepositoryPreferences.py

--- github/RepositoryPreferences.py
+++ github/RepositoryPreferences.py
@@ -24,7 +24,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import github.GithubObject
 import github.Repository
 from github.GithubObject import Attribute, NonCompletableGithubObject, NotSet
 
