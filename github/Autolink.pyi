6034233eee303b2f6656aa557671934d0a5fbb65
Jirka <jirka.borovec@seznam.cz>
2023-07-07 18:56:16 +0200

    apply + ignore

--- github/Autolink.pyi
+++ github/Autolink.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 from github.GithubObject import NonCompletableGithubObject
 
--- github/GitTag.pyi
+++ github/GitTag.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Union
+from typing import Any, Dict
 
 from github.GitAuthor import GitAuthor
 from github.GithubObject import CompletableGithubObject
