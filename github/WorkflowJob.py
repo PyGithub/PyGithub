c347f51b7e057a5ea0dd183ed014a202f5796776
Trim21 <trim21.me@gmail.com>
2023-07-08 19:03:17 +0800

    Update WorkflowJob.py

--- github/WorkflowJob.py
+++ github/WorkflowJob.py
@@ -34,80 +34,125 @@ class WorkflowJob(github.GithubObject.CompletableGithubObject):
 
     @property
     def check_run_url(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._check_run_url)
         return self._check_run_url.value
 
     @property
     def completed_at(self):
+        """
+        :type: datetime.datetime
+        """
         self._completeIfNotSet(self._completed_at)
         return self._completed_at.value
 
     @property
     def conclusion(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._conclusion)
         return self._conclusion.value
 
     @property
     def head_sha(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._head_sha)
         return self._head_sha.value
 
     @property
     def html_url(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._html_url)
         return self._html_url.value
 
     @property
     def id(self):
+        """
+        :type: int
+        """
         self._completeIfNotSet(self._id)
         return self._id.value
 
     @property
     def name(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._name)
         return self._name.value
 
     @property
     def node_id(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._node_id)
         return self._node_id.value
 
     @property
     def run_id(self):
+        """
+        :type: integer
+        """
         self._completeIfNotSet(self._run_id)
         return self._run_id.value
 
     @property
     def run_url(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._run_url)
         return self._run_url.value
 
     @property
     def started_at(self):
+        """
+        :type: datetime.datetime
+        """
         self._completeIfNotSet(self._started_at)
         return self._started_at.value
 
     @property
     def status(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._status)
         return self._status.value
 
     @property
     def steps(self):
+        """
+        :type: list of github.WorkflowStep.WorkflowStep
+        """
         self._completeIfNotSet(self._steps)
         return self._steps.value
 
     @property
     def url(self):
+        """
+        :type: string
+        """
         self._completeIfNotSet(self._url)
         return self._url.value
 
     def logs_url(self):
+        """
+        :type: string
+        """
         print(f"{self.url}/logs")
         headers, _ = self._requester.requestBlobAndCheck("GET", f"{self.url}/logs")
         return headers["location"]
 
-    def _initAttributes(self) -> None:
+    def _initAttributes(self):
         self._check_run_url = github.GithubObject.NotSet
         self._completed_at = github.GithubObject.NotSet
         self._conclusion = github.GithubObject.NotSet
@@ -123,7 +168,7 @@ class WorkflowJob(github.GithubObject.CompletableGithubObject):
         self._steps = github.GithubObject.NotSet
         self._url = github.GithubObject.NotSet
 
-    def _useAttributes(self, attributes) -> None:
+    def _useAttributes(self, attributes):
         if "check_run_url" in attributes:  # pragma no branch
             self._check_run_url = self._makeStringAttribute(attributes["check_run_url"])
         if "completed_at" in attributes:  # pragma no branch
