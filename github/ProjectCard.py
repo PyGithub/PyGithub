4fe97d035669939aa662de03a2d67786e444c900
Trim21 <trim21.me@gmail.com>
2023-07-20 02:28:00 +0800

    revert

--- github/ProjectCard.py
+++ github/ProjectCard.py
@@ -19,17 +19,12 @@
 # along with PyGithub. If not, see <http://www.gnu.org/licenses/>.             #
 #                                                                              #
 ################################################################################
-from __future__ import annotations
 
-from datetime import datetime
-from typing import Any
+from typing import Any, Dict
 
-import github.Issue
-import github.NamedUser
-import github.ProjectColumn
-import github.PullRequest
-from github import Consts
-from github.GithubObject import Attribute, CompletableGithubObject, NotSet, Opt
+import github.GithubObject
+
+from . import Consts
 
 # NOTE: There is currently no way to get cards "in triage" for a project.
 # https://platform.github.community/t/moving-github-project-cards-that-are-in-triage/3784
@@ -38,84 +33,101 @@ from github.GithubObject import Attribute, CompletableGithubObject, NotSet, Opt
 # which may point the way to where the API is likely headed and what might come back to v3. E.g. ProjectCard.content member.
 
 
-class ProjectCard(CompletableGithubObject):
+class ProjectCard(github.GithubObject.CompletableGithubObject):
     """
     This class represents Project Cards. The reference can be found here https://docs.github.com/en/rest/reference/projects#cards
     """
-
-    def _initAttributes(self) -> None:
-        self._archived: Attribute[bool] = NotSet
-        self._column_url: Attribute[str] = NotSet
-        self._content_url: Attribute[str] = NotSet
-        self._created_at: Attribute[datetime] = NotSet
-        self._creator: Attribute[github.NamedUser.NamedUser] = NotSet
-        self._id: Attribute[int] = NotSet
-        self._node_id: Attribute[str] = NotSet
-        self._note: Attribute[str] = NotSet
-        self._updated_at: Attribute[datetime] = NotSet
-        self._url: Attribute[str] = NotSet
 
     def __repr__(self) -> str:
         return self.get__repr__({"id": self._id.value})
 
     @property
-    def archived(self) -> bool:
+    def archived(self):
+        """
+        :type: bool
+        """
         return self._archived.value
 
     @property
-    def column_url(self) -> str:
+    def column_url(self):
+        """
+        :type: string
+        """
         return self._column_url.value
 
     @property
-    def content_url(self) -> str:
+    def content_url(self):
+        """
+        :type: string
+        """
         return self._content_url.value
 
     @property
-    def created_at(self) -> datetime:
+    def created_at(self):
+        """
+        :type: datetime.datetime
+        """
         return self._created_at.value
 
     @property
-    def creator(self) -> github.NamedUser.NamedUser:
+    def creator(self):
+        """
+        :type: :class:`github.NamedUser.NamedUser`
+        """
         return self._creator.value
 
     @property
-    def id(self) -> int:
+    def id(self):
+        """
+        :type: integer
+        """
         return self._id.value
 
     @property
-    def node_id(self) -> str:
+    def node_id(self):
+        """
+        :type: string
+        """
         return self._node_id.value
 
     @property
-    def note(self) -> str:
+    def note(self):
+        """
+        :type: string
+        """
         return self._note.value
 
     @property
-    def updated_at(self) -> datetime:
+    def updated_at(self):
+        """
+        :type: datetime.datetime
+        """
         return self._updated_at.value
 
     @property
-    def url(self) -> str:
+    def url(self):
+        """
+        :type: string
+        """
         return self._url.value
 
     # Note that the content_url for any card will be an "issue" URL, from
     # which you can retrieve either an Issue or a PullRequest. Unfortunately
     # the API doesn't make it clear which you are dealing with.
-    def get_content(
-        self, content_type: Opt[str] = NotSet
-    ) -> github.PullRequest.PullRequest | github.Issue.Issue | None:
+    def get_content(self, content_type=github.GithubObject.NotSet):
         """
         :calls: `GET /repos/{owner}/{repo}/pulls/{number} <https://docs.github.com/en/rest/reference/pulls#get-a-pull-request>`_
+        :param content_type: string, optional
+        :rtype: :class:`github.PullRequest.PullRequest` or :class:`github.Issue.Issue`
         """
-        assert content_type is NotSet or isinstance(content_type, str), content_type
+        assert content_type is github.GithubObject.NotSet or isinstance(content_type, str), content_type
         if self.content_url is None:
             return None
 
-        retclass: type[github.PullRequest.PullRequest] | type[github.Issue.Issue]
         if content_type == "PullRequest":
             url = self.content_url.replace("issues", "pulls")
             retclass = github.PullRequest.PullRequest
-        elif content_type is NotSet or content_type == "Issue":
+        elif content_type is github.GithubObject.NotSet or content_type == "Issue":
             url = self.content_url
             retclass = github.Issue.Issue
         else:
@@ -123,9 +135,12 @@ class ProjectCard(CompletableGithubObject):
         headers, data = self._requester.requestJsonAndCheck("GET", url)
         return retclass(self._requester, headers, data, completed=True)
 
-    def move(self, position: str, column: github.ProjectColumn.ProjectColumn | int) -> bool:
+    def move(self, position, column):
         """
         :calls: `POST /projects/columns/cards/{card_id}/moves <https://docs.github.com/en/rest/reference/projects#cards>`_
+        :param position: string
+        :param column: :class:`github.ProjectColumn.ProjectColumn` or int
+        :rtype: bool
         """
         assert isinstance(position, str), position
         assert isinstance(column, github.ProjectColumn.ProjectColumn) or isinstance(column, int), column
@@ -141,9 +156,10 @@ class ProjectCard(CompletableGithubObject):
         )
         return status == 201
 
-    def delete(self) -> bool:
+    def delete(self):
         """
         :calls: `DELETE /projects/columns/cards/{card_id} <https://docs.github.com/en/rest/reference/projects#cards>`_
+        :rtype: bool
         """
         status, _, _ = self._requester.requestJson(
             "DELETE",
@@ -152,13 +168,20 @@ class ProjectCard(CompletableGithubObject):
         )
         return status == 204
 
-    def edit(self, note: Opt[str] = NotSet, archived: Opt[bool] = NotSet) -> None:
+    def edit(self, note=github.GithubObject.NotSet, archived=github.GithubObject.NotSet):
         """
         :calls: `PATCH /projects/columns/cards/{card_id} <https://docs.github.com/en/rest/reference/projects#cards>`_
+        :param note: string
+        :param archived: bool
+        :rtype: None
         """
-        assert note is NotSet or isinstance(note, str), note
-        assert archived is NotSet or isinstance(archived, bool), archived
-        patch_parameters: dict[str, Any] = NotSet.remove_unset_items({"note": note, "archived": archived})
+        assert note is github.GithubObject.NotSet or isinstance(note, str), note
+        assert archived is github.GithubObject.NotSet or isinstance(archived, bool), archived
+        patch_parameters = dict()
+        if note is not github.GithubObject.NotSet:
+            patch_parameters["note"] = note
+        if archived is not github.GithubObject.NotSet:
+            patch_parameters["archived"] = archived
         headers, data = self._requester.requestJsonAndCheck(
             "PATCH",
             self.url,
@@ -167,7 +190,19 @@ class ProjectCard(CompletableGithubObject):
         )
         self._useAttributes(data)
 
-    def _useAttributes(self, attributes: dict[str, Any]) -> None:
+    def _initAttributes(self) -> None:
+        self._archived = github.GithubObject.NotSet
+        self._column_url = github.GithubObject.NotSet
+        self._content_url = github.GithubObject.NotSet
+        self._created_at = github.GithubObject.NotSet
+        self._creator = github.GithubObject.NotSet
+        self._id = github.GithubObject.NotSet
+        self._node_id = github.GithubObject.NotSet
+        self._note = github.GithubObject.NotSet
+        self._updated_at = github.GithubObject.NotSet
+        self._url = github.GithubObject.NotSet
+
+    def _useAttributes(self, attributes: Dict[str, Any]) -> None:
         if "archived" in attributes:  # pragma no branch
             self._archived = self._makeBoolAttribute(attributes["archived"])
         if "column_url" in attributes:  # pragma no branch
