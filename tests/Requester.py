df5ab69c3bd48f70f32ebc8c74b978a14b404c11
Hemslo Wang <hemslo.wang@gmail.com>
2023-08-19 17:21:05 +1000

    Add close to GithubIntegration
    Signed-off-by: Hemslo Wang <hemslo.wang@gmail.com>

--- tests/Requester.py
+++ tests/Requester.py
@@ -132,12 +132,30 @@ class Requester(Framework.TestCase):
             ),
         )
 
-    def testClose(self):
+    def testCloseGithub(self):
         mocked_connection = mock.MagicMock()
         mocked_custom_connection = mock.MagicMock()
 
         with github.Github() as gh:
             requester = gh._Github__requester
+            requester._Requester__connection = mocked_connection
+            requester._Requester__custom_connections.append(mocked_custom_connection)
+
+        mocked_connection.close.assert_called_once_with()
+        mocked_custom_connection.close.assert_called_once_with()
+        self.assertIsNone(requester._Requester__connection)
+
+    def testCloseGithubIntegration(self):
+        mocked_connection = mock.MagicMock()
+        mocked_custom_connection = mock.MagicMock()
+
+        class TestAuth(github.Auth.AppAuth):
+            pass
+
+        auth = TestAuth(123, "key")
+
+        with github.GithubIntegration(auth=auth) as gi:
+            requester = gi._GithubIntegration__requester
             requester._Requester__connection = mocked_connection
             requester._Requester__custom_connections.append(mocked_custom_connection)
 
