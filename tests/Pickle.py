d82b484f4b6453f626311bf24df8868f5770ab71
Hemslo Wang <hemslo.wang@gmail.com>
2023-08-24 00:07:26 +1000

    Use lazy repo in pickle test
    Signed-off-by: Hemslo Wang <hemslo.wang@gmail.com>

--- tests/Pickle.py
+++ tests/Pickle.py
@@ -7,8 +7,8 @@ from github.Repository import Repository
 REPO_NAME = "PyGithub/PyGithub"
 
 
-class TestPickle(unittest.TestCase):
-    def test_pickle_github(self):
+class Pickle(unittest.TestCase):
+    def testPickleGithub(self):
         gh = github.Github()
         gh2 = pickle.loads(pickle.dumps(gh))
         self.assertIsInstance(gh2, github.Github)
@@ -16,9 +16,9 @@ class TestPickle(unittest.TestCase):
         self.assertIsNone(gh2._Github__requester._Requester__connection)
         self.assertEqual(len(gh2._Github__requester._Requester__custom_connections), 0)
 
-    def test_pickle_repository(self):
+    def testPickleRepository(self):
         gh = github.Github()
-        repo = gh.get_repo(REPO_NAME)
+        repo = gh.get_repo(REPO_NAME, lazy=True)
         repo2 = pickle.loads(pickle.dumps(repo))
         self.assertIsInstance(repo2, Repository)
         self.assertIsNotNone(repo2._requester._Requester__connection_lock)
