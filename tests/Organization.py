603896f44541740d72aa441ed60c698760a1393e
Mohamed Mostafa <112487260+mohy01@users.noreply.github.com>
2024-01-02 00:32:15 +0200

    Add and update organisation dependabot secrets (#2316)

--- tests/Organization.py
+++ tests/Organization.py
@@ -448,6 +448,15 @@ class Organization(Framework.TestCase):
         secrets = self.org.get_secrets()
         self.assertEqual(len(list(secrets)), 1)
 
+    def testGetDependabotSecrets(self):
+        secrets = self.org.get_secrets(secret_type="dependabot")
+        self.assertEqual(len(list(secrets)), 1)
+
+    def testGetSecretsFail(self):
+        with self.assertRaises(AssertionError) as raisedexp:
+            self.org.get_secrets(secret_type="secret")
+        self.assertEqual("secret_type should be actions or dependabot", str(raisedexp.exception))
+
     def testInviteUserWithNeither(self):
         with self.assertRaises(AssertionError) as raisedexp:
             self.org.invite_user()
