e1682e8ef1c29fb4d79abf3e1fede316e603e9a0
Sean Killen <sean@killens.co.uk>
2024-01-17 18:54:30 +0000

    Fixing tests , updating Repository Public Key config

--- tests/Repository.py
+++ tests/Repository.py
@@ -484,14 +484,14 @@ class Repository(Framework.TestCase):
     def testCreateActionsSecret(self, encrypt):
         # encrypt returns a non-deterministic value, we need to mock it so the replay data matches
         encrypt.return_value = "M+5Fm/BqTfB90h3nC7F3BoZuu3nXs+/KtpXwxm9gG211tbRo0F5UiN0OIfYT83CKcx9oKES9Va4E96/b"
-        secret = self.repo.create_secret("secret-name", "secret-value")
+        secret = self.repo.create_secret("secret-name", "secret-value", "actions")
         self.assertIsNotNone(secret)
 
     @mock.patch("github.PublicKey.encrypt")
     def testCreateDependabotSecret(self, encrypt):
         # encrypt returns a non-deterministic value, we need to mock it so the replay data matches
         encrypt.return_value = "M+5Fm/BqTfB90h3nC7F3BoZuu3nXs+/KtpXwxm9gG211tbRo0F5UiN0OIfYT83CKcx9oKES9Va4E96/b"
-        secret = self.repo.create_secret("secret-name", "secret-value", secret_type="dependabot")
+        secret = self.repo.create_secret("secret-name", "secret-value", "dependabot")
         self.assertIsNotNone(secret)
 
     @mock.patch("github.PublicKey.encrypt")
