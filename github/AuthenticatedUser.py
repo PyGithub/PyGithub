61b7b94d4920ef822dcb5e7490fec4e939a00797
xmo-odoo <xmo@odoo.com>
2024-04-08 09:33:48 +0200

    Resolve login when repr-ing AuthenticatedUser
    It's extremely confusing to dump an `AuthenticatedUser` and get `AuthenticatedUser(login=None)` even though the `AuthenticatedUser` is correctly set up, and necessarily has a login.
    
    This risks incurring additional requests, but I think in the contexts where it would be used (mostly at the shell or when debugging services) it's more helpful to actually show the information.

--- github/AuthenticatedUser.py
+++ github/AuthenticatedUser.py
@@ -177,7 +177,7 @@ class AuthenticatedUser(CompletableGithubObject):
         self._two_factor_authentication: Attribute[bool] = NotSet
 
     def __repr__(self) -> str:
-        return self.get__repr__({"login": self._login.value})
+        return self.get__repr__({"login": self.login})
 
     @property
     def avatar_url(self) -> str:
