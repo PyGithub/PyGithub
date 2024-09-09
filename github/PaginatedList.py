ed9c3552b1a1e94d683426e63f73dce0b6ade6eb
Sam <35731946+sam93210@users.noreply.github.com>
2024-09-09 19:00:49 +0300

    Update PaginatedList.py

--- github/PaginatedList.py
+++ github/PaginatedList.py
@@ -52,6 +52,7 @@
 from typing import Any, Callable, Dict, Generic, Iterator, List, Optional, Type, TypeVar, Union
 from urllib.parse import parse_qs
 
+from github import Consts
 from github.GithubObject import GithubObject
 from github.Requester import Requester
 
