180357f33312dc2e8746932fb4c8025e60c0f751
Enrico Minack <github@enrico.minack.dev>
2024-12-13 08:47:47 +0100

    Fix assertion of attributes of None instances

--- scripts/update-assertions.sh
+++ scripts/update-assertions.sh
@@ -19,31 +19,41 @@ update_assertion() {
   if [[ -z "$assertion_line" ]]; then return; fi
   #echo $assertion_line
   #echo $line_number_line
-  assertion_line="${assertion_line/E       AssertionError: /}"
-  actual="${assertion_line/% != */}"
-  expected="${assertion_line/#* != /}"
   line_number="${line_number_line/$test_file:/}"
   line_number="${line_number/%: */}"
-  actual="${actual//datetime\./}"
-  #echo "$actual" >&2
-  #echo "$expected" >&2
-  #echo "$line_number" >&2
   echo "$line_number"
-  if [ "$actual" == "None" ]; then
-    sed -i -e "${line_number}s/\S*[(]\([^,]*\),.*/self.assertIsNone(\1)/" "$test_file"
-  elif [[ "$actual" == "'"*"["*" chars]"*"'" ]]; then
-    prefix="${actual%%[*}"
-    repl=$(sed -e "s/\([$.*[\/^]\)/\\\\\1/g" <<< "$prefix${prefix:0:1}")
-    sed -i -e "${line_number}s/\S*[(]\([^,]*\), .*[)]/self.assertTrue(\1.startswith($repl))/" "$test_file"
+
+  if [[ "$assertion_line" == *"AttributeError"* ]]; then
+    assertion_line="${assertion_line/E       AttributeError: /}"
+    if [[ "$assertion_line" == "'NoneType' object has no attribute "* ]]; then
+      attribute="${assertion_line/\'NoneType\' object has no attribute /}"
+      attribute="${attribute//\'/}"
+      sed -i -e "${line_number}s/\S*[(]\([^,]*\).$attribute,.*/self.assertIsNone(\1)/" "$test_file"
+    fi
   else
-    repl=$(sed -e "s/\([$.*[\/^]\)/\\\\\1/g" <<< "$actual")
-    sed -i -e "${line_number}s/[(]\([^,]*\), .*[)]/(\1, $repl)/" "$test_file"
+    assertion_line="${assertion_line/E       AssertionError: /}"
+    actual="${assertion_line/% != */}"
+    expected="${assertion_line/#* != /}"
+    actual="${actual//datetime\./}"
+    #echo "$actual" >&2
+    #echo "$expected" >&2
+    #echo "$line_number" >&2
+    if [ "$actual" == "None" ]; then
+      sed -i -e "${line_number}s/\S*[(]\([^,]*\),.*/self.assertIsNone(\1)/" "$test_file"
+    elif [[ "$actual" == "'"*"["*" chars]"*"'" ]]; then
+      prefix="${actual%%[*}"
+      repl=$(sed -e "s/\([$.*[\/^]\)/\\\\\1/g" <<< "$prefix${prefix:0:1}")
+      sed -i -e "${line_number}s/\S*[(]\([^,]*\), .*[)]/self.assertTrue(\1.startswith($repl))/" "$test_file"
+    else
+      repl=$(sed -e "s/\([$.*[\/^]\)/\\\\\1/g" <<< "$actual")
+      sed -i -e "${line_number}s/[(]\([^,]*\), .*[)]/(\1, $repl)/" "$test_file"
+    fi
   fi
 }
 
 last_line_number=
 while true; do
-  line_number=$($pytest --color=no "$test_file" -k "$test_func" | grep AssertionError | update_assertion || true)
+  line_number=$($pytest --color=no "$test_file" -k "$test_func" | grep -e AttributeError -e AssertionError | update_assertion || true)
   if [[ "$line_number" == "" ]]; then exit; fi
   if [[ "$line_number" == "$last_line_number" ]]; then
     echo "Could not fix assertion in line $line_number"
