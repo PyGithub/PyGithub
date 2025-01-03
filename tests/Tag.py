fc64f3ac26c5175d1b9a272be7193a415f6d6335
Enrico Minack <github@enrico.minack.dev>
2025-01-03 12:36:42 +0100

    Updated test assertions

--- tests/Tag.py
+++ tests/Tag.py
@@ -45,7 +45,7 @@ class Tag(Framework.TestCase):
     def testAttributes(self):
         self.assertEqual(self.tag.commit.sha, "636e6112deb72277b3bffcc3303cd7e8a7431a5d")
         self.assertEqual(self.tag.name, "v0.3")
-        self.assertEqual(self.tag.node_id, "")
+        self.assertIsNone(self.tag.node_id)
         self.assertEqual(self.tag.tarball_url, "https://github.com/jacquev6/PyGithub/tarball/v0.3")
         self.assertEqual(self.tag.zipball_url, "https://github.com/jacquev6/PyGithub/zipball/v0.3")
         self.assertEqual(
