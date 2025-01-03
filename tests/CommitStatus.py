5b6fea797acf24e7493b5452081e9c8b307ecff5
Enrico Minack <github@enrico.minack.dev>
2025-01-03 17:27:45 +0100

    Give test CommitStatus a test attribute

--- tests/CommitStatus.py
+++ tests/CommitStatus.py
@@ -45,29 +45,30 @@ class CommitStatus(Framework.TestCase):
         self.statuses = list(
             self.g.get_user().get_repo("PyGithub").get_commit("1292bf0e22c796e91cc3d6e24b544aece8c21f2a").get_statuses()
         )
+        self.status = self.statuses[0]
 
     def testAttributes(self):
         self.assertEqual(
-            self.statuses[0].created_at,
+            self.status.created_at,
             datetime(2012, 9, 8, 11, 30, 56, tzinfo=timezone.utc),
         )
         self.assertEqual(
-            self.statuses[0].updated_at,
+            self.status.updated_at,
             datetime(2012, 9, 8, 11, 30, 56, tzinfo=timezone.utc),
         )
-        self.assertEqual(self.statuses[0].creator.login, "jacquev6")
-        self.assertEqual(self.statuses[0].description, "Status successfully created by PyGithub")
+        self.assertEqual(self.status.creator.login, "jacquev6")
+        self.assertEqual(self.status.description, "Status successfully created by PyGithub")
         self.assertEqual(self.statuses[1].description, None)
-        self.assertEqual(self.statuses[0].id, 277040)
-        self.assertEqual(self.statuses[0].state, "success")
+        self.assertEqual(self.status.id, 277040)
+        self.assertEqual(self.status.state, "success")
         self.assertEqual(self.statuses[1].state, "pending")
-        self.assertEqual(self.statuses[0].context, "build")
+        self.assertEqual(self.status.context, "build")
         self.assertEqual(
-            self.statuses[0].target_url,
+            self.status.target_url,
             "https://github.com/jacquev6/PyGithub/issues/67",
         )
         self.assertEqual(self.statuses[1].target_url, None)
         self.assertEqual(
-            repr(self.statuses[0]),
+            repr(self.status),
             'CommitStatus(state="success", id=277040, context="build")',
         )
