f60a8c0b4e0f3e8991855195be6aed652171cd34
Enrico Minack <github@enrico.minack.dev>
2025-01-03 12:27:43 +0100

    Updated test class according to API spec

--- tests/GithubApp.py
+++ tests/GithubApp.py
@@ -24,6 +24,8 @@
 #                                                                              #
 ################################################################################
 
+from __future__ import annotations
+
 from datetime import datetime, timezone
 
 import github
