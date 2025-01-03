5ec6ed26f45be1ef1a6a6badc53b0db90098eaf4
Enrico Minack <github@enrico.minack.dev>
2025-01-03 12:37:16 +0100

    Updated test class according to API spec

--- tests/UserKey.py
+++ tests/UserKey.py
@@ -32,7 +32,10 @@
 #                                                                              #
 ################################################################################
 
+from __future__ import annotations
+
 from . import Framework
+from datetime import datetime, timezone
 
 
 class UserKey(Framework.TestCase):
@@ -41,11 +44,13 @@ class UserKey(Framework.TestCase):
         self.key = self.g.get_user().get_key(2626650)
 
     def testAttributes(self):
+        self.assertEqual(self.key.created_at, datetime(2020, 1, 2, 12, 34, 56, tzinfo=timezone.utc))
         self.assertEqual(self.key.id, 2626650)
         self.assertEqual(
             self.key.key,
             "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA2Mm0RjTNAYFfSCtUpO54usdseroUSIYg5KX4JoseTpqyiB/hqewjYLAdUq/tNIQzrkoEJWSyZrQt0ma7/YCyMYuNGd3DU6q6ZAyBeY3E9RyCiKjO3aTL2VKQGFvBVVmGdxGVSCITRphAcsKc/PF35/fg9XP9S0anMXcEFtdfMHz41SSw+XtE+Vc+6cX9FuI5qUfLGbkv8L1v3g4uw9VXlzq4GfTA+1S7D6mcoGHopAIXFlVr+2RfDKdSURMcB22z41fljO1MW4+zUS/4FyUTpL991es5fcwKXYoiE+x06VJeJJ1Krwx+DZj45uweV6cHXt2JwJEI9fWB6WyBlDejWw==",
         )
+        self.assertEqual(self.key.read_only, False)
         self.assertEqual(self.key.title, "Key added through PyGithub")
         self.assertEqual(self.key.url, "https://api.github.com/user/keys/2626650")
         self.assertTrue(self.key.verified)
