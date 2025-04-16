057cdd86be95a284968ef9cafc16467eb1cd5457
Oscar van Leusen <oscarvanleusen@gmail.com>
2025-04-16 14:25:52 +0100

    Run black, fix end-of-file-fixer

--- github/Repository.py
+++ github/Repository.py
@@ -1268,7 +1268,7 @@ class Repository(CompletableGithubObject):
         if isinstance(collaborator, github.NamedUser.NamedUser):
             collaborator = collaborator._identity
         else:
-            collaborator = urllib.parse.quote(collaborator, safe='')
+            collaborator = urllib.parse.quote(collaborator, safe="")
 
         if is_defined(permission):
             put_parameters = {"permission": permission}
@@ -1294,7 +1294,7 @@ class Repository(CompletableGithubObject):
         if isinstance(collaborator, github.NamedUser.NamedUser):
             collaborator = collaborator._identity
         else:
-            collaborator = urllib.parse.quote(collaborator, safe='')
+            collaborator = urllib.parse.quote(collaborator, safe="")
         headers, data = self._requester.requestJsonAndCheck(
             "GET",
             f"{self.url}/collaborators/{collaborator}/permission",
@@ -1932,7 +1932,7 @@ class Repository(CompletableGithubObject):
         assert isinstance(unencrypted_value, str), unencrypted_value
         assert secret_type in ["actions", "dependabot"], "secret_type should be actions or dependabot"
 
-        secret_name = urllib.parse.quote(secret_name, safe='')
+        secret_name = urllib.parse.quote(secret_name, safe="")
         public_key = self.get_public_key(secret_type=secret_type)
         payload = public_key.encrypt(unencrypted_value)
         put_parameters = {
@@ -1980,7 +1980,7 @@ class Repository(CompletableGithubObject):
         assert isinstance(secret_name, str), secret_name
         assert secret_type in ["actions", "dependabot"], "secret_type should be actions or dependabot"
 
-        secret_name = urllib.parse.quote(secret_name, safe='')
+        secret_name = urllib.parse.quote(secret_name, safe="")
         return github.Secret.Secret(
             requester=self._requester,
             headers={},
@@ -2030,7 +2030,7 @@ class Repository(CompletableGithubObject):
         :rtype: github.Variable.Variable
         """
         assert isinstance(variable_name, str), variable_name
-        variable_name = urllib.parse.quote(variable_name, safe='')
+        variable_name = urllib.parse.quote(variable_name, safe="")
         return github.Variable.Variable(
             requester=self._requester,
             headers={},
@@ -2047,7 +2047,7 @@ class Repository(CompletableGithubObject):
         """
         assert isinstance(secret_name, str), secret_name
         assert secret_type in ["actions", "dependabot"], "secret_type should be actions or dependabot"
-        secret_name = urllib.parse.quote(secret_name, safe='')
+        secret_name = urllib.parse.quote(secret_name, safe="")
         status, headers, data = self._requester.requestJson("DELETE", f"{self.url}/{secret_type}/secrets/{secret_name}")
         return status == 204
 
@@ -2058,7 +2058,7 @@ class Repository(CompletableGithubObject):
         :rtype: bool
         """
         assert isinstance(variable_name, str), variable_name
-        variable_name = urllib.parse.quote(variable_name, safe='')
+        variable_name = urllib.parse.quote(variable_name, safe="")
         status, headers, data = self._requester.requestJson("DELETE", f"{self.url}/actions/variables/{variable_name}")
         return status == 204
 
@@ -2209,11 +2209,11 @@ class Repository(CompletableGithubObject):
         :rtype: string
         """
         assert isinstance(archive_format, str), archive_format
-        archive_format = urllib.parse.quote(archive_format, safe='')
+        archive_format = urllib.parse.quote(archive_format, safe="")
         assert is_optional(ref, str), ref
         url = f"{self.url}/{archive_format}"
         if is_defined(ref):
-            ref = urllib.parse.quote(ref, safe='')
+            ref = urllib.parse.quote(ref, safe="")
             url += f"/{ref}"
         headers, data = self._requester.requestJsonAndCheck("GET", url)
         return headers["location"]
@@ -2993,7 +2993,7 @@ class Repository(CompletableGithubObject):
         :rtype: :class:`PaginatedList` of :class:`github.GitRef.GitRef`
         """
         assert isinstance(ref, str), ref
-        ref = urllib.parse.quote(ref, safe='')
+        ref = urllib.parse.quote(ref, safe="")
         return PaginatedList(
             github.GitRef.GitRef,
             self._requester,
@@ -3008,7 +3008,7 @@ class Repository(CompletableGithubObject):
         :rtype: :class:`github.GitTag.GitTag`
         """
         assert isinstance(sha, str), sha
-        sha = urllib.parse.quote(sha, safe='')
+        sha = urllib.parse.quote(sha, safe="")
         headers, data = self._requester.requestJsonAndCheck("GET", f"{self.url}/git/tags/{sha}")
         return github.GitTag.GitTag(self._requester, headers, data, completed=True)
 
@@ -3239,7 +3239,9 @@ class Repository(CompletableGithubObject):
         :rtype: :class:`github.Label.Label`
         """
         assert isinstance(name, str), name
-        headers, data = self._requester.requestJsonAndCheck("GET", f"{self.url}/labels/{urllib.parse.quote(name, safe='')}")
+        headers, data = self._requester.requestJsonAndCheck(
+            "GET", f"{self.url}/labels/{urllib.parse.quote(name, safe='')}"
+        )
         return github.Label.Label(self._requester, headers, data, completed=True)
 
     def get_labels(self) -> PaginatedList[Label]:
@@ -3590,7 +3592,7 @@ class Repository(CompletableGithubObject):
             headers, data = self._requester.requestJsonAndCheck("GET", f"{self.url}/releases/{id}")
             return github.GitRelease.GitRelease(self._requester, headers, data, completed=True)
         elif isinstance(id, str):
-            id = urllib.parse.quote(id, safe='')
+            id = urllib.parse.quote(id, safe="")
             headers, data = self._requester.requestJsonAndCheck("GET", f"{self.url}/releases/tags/{id}")
             return github.GitRelease.GitRelease(self._requester, headers, data, completed=True)
 
@@ -3648,7 +3650,7 @@ class Repository(CompletableGithubObject):
         :rtype: :class:`github.Workflow.Workflow`
         """
         assert isinstance(id_or_file_name, (int, str)), id_or_file_name
-        id_or_file_name = urllib.parse.quote(str(id_or_file_name), safe='')
+        id_or_file_name = urllib.parse.quote(str(id_or_file_name), safe="")
         headers, data = self._requester.requestJsonAndCheck("GET", f"{self.url}/actions/workflows/{id_or_file_name}")
         return github.Workflow.Workflow(self._requester, headers, data, completed=True)
 
@@ -3738,7 +3740,7 @@ class Repository(CompletableGithubObject):
         if isinstance(assignee, github.NamedUser.NamedUser):
             assignee = assignee._identity
         else:
-            assignee = urllib.parse.quote(assignee, safe='')
+            assignee = urllib.parse.quote(assignee, safe="")
 
         status, headers, data = self._requester.requestJson("GET", f"{self.url}/assignees/{assignee}")
         return status == 204
@@ -3754,7 +3756,7 @@ class Repository(CompletableGithubObject):
         if isinstance(collaborator, github.NamedUser.NamedUser):
             collaborator = collaborator._identity
         else:
-            collaborator = urllib.parse.quote(collaborator, safe='')
+            collaborator = urllib.parse.quote(collaborator, safe="")
 
         status, headers, data = self._requester.requestJson("GET", f"{self.url}/collaborators/{collaborator}")
         return status == 204
@@ -3962,7 +3964,7 @@ class Repository(CompletableGithubObject):
         if isinstance(collaborator, github.NamedUser.NamedUser):
             collaborator = collaborator._identity
         else:
-            collaborator = urllib.parse.quote(collaborator, safe='')
+            collaborator = urllib.parse.quote(collaborator, safe="")
 
         headers, data = self._requester.requestJsonAndCheck("DELETE", f"{self.url}/collaborators/{collaborator}")
 
@@ -4066,7 +4068,7 @@ class Repository(CompletableGithubObject):
         assert isinstance(event, str), event
         assert isinstance(callback, str), callback
         assert is_optional(secret, str), secret
-        event = urllib.parse.quote(event, safe='')
+        event = urllib.parse.quote(event, safe="")
 
         post_parameters = collections.OrderedDict()
         post_parameters["hub.callback"] = callback
@@ -4218,7 +4220,7 @@ class Repository(CompletableGithubObject):
         :rtype: :class:`github.Environment.Environment`
         """
         assert isinstance(environment_name, str), environment_name
-        environment_name = urllib.parse.quote(environment_name, safe='')
+        environment_name = urllib.parse.quote(environment_name, safe="")
         headers, data = self._requester.requestJsonAndCheck("GET", f"{self.url}/environments/{environment_name}")
         data["environments_url"] = f"/repositories/{self.id}/environments"
         return Environment(self._requester, headers, data, completed=True)
@@ -4251,7 +4253,7 @@ class Repository(CompletableGithubObject):
             )
             or deployment_branch_policy is None
         )
-        environment_name = urllib.parse.quote(environment_name, safe='')
+        environment_name = urllib.parse.quote(environment_name, safe="")
 
         put_parameters = {
             "wait_timer": wait_timer,
@@ -4272,7 +4274,7 @@ class Repository(CompletableGithubObject):
         :rtype: None
         """
         assert isinstance(environment_name, str), environment_name
-        environment_name = urllib.parse.quote(environment_name, safe='')
+        environment_name = urllib.parse.quote(environment_name, safe="")
 
         headers, data = self._requester.requestJsonAndCheck("DELETE", f"{self.url}/environments/{environment_name}")
 
