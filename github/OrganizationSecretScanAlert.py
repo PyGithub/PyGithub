c130ba3c7c1411fb070eb8c45ebb15dec5426e8e
Matthew Davis <matt.w.davis@gmail.com>
2025-08-22 17:36:14 -0700

    Fixed dependency issue on Lint mypy check

--- github/OrganizationSecretScanAlert.py
+++ github/OrganizationSecretScanAlert.py
@@ -22,11 +22,15 @@
 
 from __future__ import annotations
 
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
-from github.SecretScanAlert import SecretScanAlert
+import github.SecretScanAlert
+import github.Repository
 from github.GithubObject import Attribute, NotSet
-from github.Repository import Repository
+
+if TYPE_CHECKING:
+    from github.SecretScanAlert import SecretScanAlert
+    from github.Repository import Repository
 
 
 class OrganizationSecretScanAlert(SecretScanAlert):
