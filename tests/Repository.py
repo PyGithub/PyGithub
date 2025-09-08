820268e44f6dc79bcf77f961d56d9e45c22c8ae3
Sandra Parsick <sandra@parsick.dev>
2025-09-08 11:11:40 +0200

    Add rulesets.

--- tests/Repository.py
+++ tests/Repository.py
@@ -80,6 +80,7 @@
 # Copyright 2025 Mikhail f. Shiryaev <mr.felixoid@gmail.com>                   #
 # Copyright 2025 Tan An Nie <121005973+tanannie22@users.noreply.github.com>    #
 # Copyright 2025 Zdenek Styblik <6183869+zstyblik@users.noreply.github.com>    #
+# Copyright 2025 Jens Keiner <jens.keiner@gmail.com>                           #
 #                                                                              #
 # This file is part of PyGithub.                                               #
 # http://pygithub.readthedocs.io/                                              #
@@ -2295,6 +2296,280 @@ class Repository(Framework.TestCase):
                 "status": "422",
             },
         )
+
+    def testGetRulesForBranch(self):
+        rules = self.repo.get_rules_for_branch("main")
+        self.assertEqual(rules.totalCount, 7)
+        rule = rules[0]
+        self.assertEqual(rule.type, "deletion")
+        self.assertEqual(rule.ruleset_source_type, "Repository")
+        self.assertEqual(rule.ruleset_source, "PyGithub/PyGithub")
+        self.assertEqual(rule.ruleset_id, 3474546)
+        rule = rules[1]
+        self.assertEqual(rule.type, "non_fast_forward")
+        self.assertEqual(rule.ruleset_source_type, "Repository")
+        self.assertEqual(rule.ruleset_source, "PyGithub/PyGithub")
+        self.assertEqual(rule.ruleset_id, 3474546)
+        rule = rules[2]
+        self.assertEqual(rule.type, "creation")
+        self.assertEqual(rule.ruleset_source_type, "Repository")
+        self.assertEqual(rule.ruleset_source, "PyGithub/PyGithub")
+        self.assertEqual(rule.ruleset_id, 3474546)
+        rule = rules[3]
+        self.assertEqual(rule.type, "update")
+        self.assertEqual(rule.parameters, {"update_allows_fetch_and_merge": True})
+        self.assertEqual(rule.ruleset_source_type, "Repository")
+        self.assertEqual(rule.ruleset_source, "PyGithub/PyGithub")
+        self.assertEqual(rule.ruleset_id, 3474546)
+        rule = rules[4]
+        self.assertEqual(rule.type, "required_signatures")
+        self.assertEqual(rule.ruleset_source_type, "Repository")
+        self.assertEqual(rule.ruleset_source, "PyGithub/PyGithub")
+        self.assertEqual(rule.ruleset_id, 3474546)
+        rule = rules[5]
+        self.assertEqual(rule.type, "pull_request")
+        self.assertEqual(
+            rule.parameters,
+            {
+                "required_approving_review_count": 0,
+                "dismiss_stale_reviews_on_push": False,
+                "require_code_owner_review": False,
+                "require_last_push_approval": False,
+                "required_review_thread_resolution": False,
+                "automatic_copilot_code_review_enabled": False,
+                "allowed_merge_methods": ["merge", "squash", "rebase"],
+            },
+        )
+        self.assertEqual(rule.ruleset_source_type, "Repository")
+        self.assertEqual(rule.ruleset_source, "PyGithub/PyGithub")
+        self.assertEqual(rule.ruleset_id, 3474546)
+        rule = rules[6]
+        self.assertEqual(rule.type, "required_signatures")
+        self.assertEqual(rule.ruleset_source_type, "Repository")
+        self.assertEqual(rule.ruleset_source, "PyGithub/PyGithub")
+        self.assertEqual(rule.ruleset_id, 3474551)
+
+    def testGetRulesets(self):
+        rulesets = self.repo.get_rulesets()
+        self.assertEqual(rulesets.totalCount, 2)
+        rule = rulesets[0]
+        self.assertEqual(
+            rule._links,
+            {
+                "self": {"href": "https://api.github.com/repos/PyGithub/PyGithub/rulesets/3474546"},
+                "html": {"href": "https://github.com/PyGithub/PyGithub/rules/3474546"},
+            },
+        )
+        self.assertEqual(rule.id, 3474546)
+        self.assertEqual(rule.name, "main protection")
+        self.assertEqual(rule.target, "branch")
+        self.assertEqual(rule.source_type, "Repository")
+        self.assertEqual(rule.source, "PyGithub/PyGithub")
+        self.assertEqual(rule.enforcement, "active")
+        self.assertEqual(rule.node_id, "RRS_lACqUmVwb3NpdG9yec43HleVzgA1BHI")
+        self.assertEqual(rule.created_at, datetime.fromisoformat("2025-01-30T22:27:33.194+01:00"))
+        self.assertEqual(rule.updated_at, datetime.fromisoformat("2025-01-30T22:27:33.194+01:00"))
+        rule = rulesets[1]
+        self.assertEqual(
+            rule._links,
+            {
+                "self": {"href": "https://api.github.com/repos/PyGithub/PyGithub/rulesets/3474551"},
+                "html": {"href": "https://github.com/PyGithub/PyGithub/rules/3474551"},
+            },
+        )
+        self.assertEqual(rule.id, 3474551)
+        self.assertEqual(rule.name, "require signed commits")
+        self.assertEqual(rule.target, "branch")
+        self.assertEqual(rule.source_type, "Repository")
+        self.assertEqual(rule.source, "PyGithub/PyGithub")
+        self.assertEqual(rule.enforcement, "active")
+        self.assertEqual(rule.node_id, "RRS_lACqUmVwb3NpdG9yec43HleVzgA1BHc")
+        self.assertEqual(rule.created_at, datetime.fromisoformat("2025-01-30T22:29:09.256+01:00"))
+        self.assertEqual(rule.updated_at, datetime.fromisoformat("2025-01-30T22:29:09.256+01:00"))
+
+    def testGetRulesetsWithCompletion(self):
+        rulesets = self.repo.get_rulesets()
+        self.assertEqual(rulesets.totalCount, 2)
+        ruleset = rulesets[0]
+        # Force completion by validating all properties.
+        self.assertEqual(ruleset.id, 3474546)
+        self.assertEqual(ruleset.name, "main protection")
+        self.assertEqual(ruleset.target, "branch")
+        self.assertEqual(ruleset.source_type, "Repository")
+        self.assertEqual(ruleset.source, "PyGithub/PyGithub")
+        self.assertEqual(ruleset.enforcement, "active")
+        self.assertEqual(ruleset.node_id, "RRS_lACqUmVwb3NpdG9yec43HleVzgA1BHI")
+        self.assertEqual(ruleset.created_at, datetime.fromisoformat("2025-01-30T22:27:33.194+01:00"))
+        self.assertEqual(ruleset.updated_at, datetime.fromisoformat("2025-01-30T22:27:33.194+01:00"))
+        self.assertEqual(
+            ruleset.conditions, {"ref_name": {"exclude": [], "include": ["refs/heads/master", "refs/heads/main"]}}
+        )
+        self.assertEqual(len(ruleset.rules), 6)
+        self.assertEqual(ruleset.rules[0].type, "deletion")
+        self.assertEqual(ruleset.rules[1].type, "non_fast_forward")
+        self.assertEqual(ruleset.rules[2].type, "creation")
+        self.assertEqual(ruleset.rules[3].type, "update")
+        self.assertEqual(ruleset.rules[3].parameters, {"update_allows_fetch_and_merge": True})
+        self.assertEqual(ruleset.rules[4].type, "required_signatures")
+        self.assertEqual(ruleset.rules[5].type, "pull_request")
+        self.assertEqual(
+            ruleset.rules[5].parameters,
+            {
+                "required_approving_review_count": 0,
+                "dismiss_stale_reviews_on_push": False,
+                "require_code_owner_review": False,
+                "require_last_push_approval": False,
+                "required_review_thread_resolution": False,
+                "automatic_copilot_code_review_enabled": False,
+                "allowed_merge_methods": ["merge", "squash", "rebase"],
+            },
+        )
+        self.assertEqual(len(ruleset.bypass_actors), 1)
+        self.assertEqual(
+            ruleset.bypass_actors[0], {"actor_id": 5, "actor_type": "RepositoryRole", "bypass_mode": "always"}
+        )
+        self.assertEqual(ruleset.current_user_can_bypass, "always")
+        self.assertEqual(
+            ruleset._links,
+            {
+                "self": {"href": "https://api.github.com/repos/PyGithub/PyGithub/rulesets/3474546"},
+                "html": {"href": "https://github.com/PyGithub/PyGithub/rules/3474546"},
+            },
+        )
+
+    def testGetRuleset(self):
+        ruleset = self.repo.get_ruleset(3474546)
+        self.assertEqual(ruleset.id, 3474546)
+        self.assertEqual(ruleset.name, "main protection")
+        self.assertEqual(ruleset.target, "branch")
+        self.assertEqual(ruleset.source_type, "Repository")
+        self.assertEqual(ruleset.source, "PyGithub/PyGithub")
+        self.assertEqual(ruleset.enforcement, "active")
+        self.assertEqual(ruleset.node_id, "RRS_lACqUmVwb3NpdG9yec43HleVzgA1BHI")
+        self.assertEqual(ruleset.created_at, datetime.fromisoformat("2025-01-30T22:27:33.194+01:00"))
+        self.assertEqual(ruleset.updated_at, datetime.fromisoformat("2025-01-30T22:27:33.194+01:00"))
+        self.assertEqual(
+            ruleset.conditions, {"ref_name": {"exclude": [], "include": ["refs/heads/master", "refs/heads/main"]}}
+        )
+        self.assertEqual(len(ruleset.rules), 6)
+        self.assertEqual(ruleset.rules[0].type, "deletion")
+        self.assertEqual(ruleset.rules[1].type, "non_fast_forward")
+        self.assertEqual(ruleset.rules[2].type, "creation")
+        self.assertEqual(ruleset.rules[3].type, "update")
+        self.assertEqual(ruleset.rules[3].parameters, {"update_allows_fetch_and_merge": True})
+        self.assertEqual(ruleset.rules[4].type, "required_signatures")
+        self.assertEqual(ruleset.rules[5].type, "pull_request")
+        self.assertEqual(
+            ruleset.rules[5].parameters,
+            {
+                "required_approving_review_count": 0,
+                "dismiss_stale_reviews_on_push": False,
+                "require_code_owner_review": False,
+                "require_last_push_approval": False,
+                "required_review_thread_resolution": False,
+                "automatic_copilot_code_review_enabled": False,
+                "allowed_merge_methods": ["merge", "squash", "rebase"],
+            },
+        )
+        self.assertEqual(len(ruleset.bypass_actors), 1)
+        self.assertEqual(
+            ruleset.bypass_actors[0], {"actor_id": 5, "actor_type": "RepositoryRole", "bypass_mode": "always"}
+        )
+        self.assertEqual(ruleset.current_user_can_bypass, "always")
+        self.assertEqual(
+            ruleset._links,
+            {
+                "self": {"href": "https://api.github.com/repos/PyGithub/PyGithub/rulesets/3474546"},
+                "html": {"href": "https://github.com/PyGithub/PyGithub/rules/3474546"},
+            },
+        )
+
+    def testCreateRuleset(self):
+        ruleset = self.repo.create_ruleset(
+            name="new rule set",
+            target="branch",
+            enforcement="active",
+            bypass_actors=[{"actor_id": 5, "actor_type": "RepositoryRole", "bypass_mode": "always"}],
+            conditions={"ref_name": {"exclude": [], "include": ["refs/heads/master", "refs/heads/main"]}},
+            rules=[
+                {"type": "deletion"},
+                {"type": "non_fast_forward"},
+                {"type": "creation"},
+            ],
+        )
+        self.assertEqual(ruleset.id, 3490869)
+        self.assertEqual(ruleset.name, "new rule set")
+        self.assertEqual(ruleset.target, "branch")
+        self.assertEqual(ruleset.source_type, "Repository")
+        self.assertEqual(ruleset.source, "PyGithub/PyGithub")
+        self.assertEqual(ruleset.enforcement, "active")
+        self.assertEqual(ruleset.node_id, "RRS_lACqUmVwb3NpdG9yec43HleVzgA1RDU")
+        self.assertEqual(ruleset.created_at, datetime.fromisoformat("2025-01-31T20:59:12.515+01:00"))
+        self.assertEqual(ruleset.updated_at, datetime.fromisoformat("2025-01-31T20:59:12.515+01:00"))
+        self.assertEqual(
+            ruleset.conditions, {"ref_name": {"exclude": [], "include": ["refs/heads/master", "refs/heads/main"]}}
+        )
+        self.assertEqual(len(ruleset.rules), 3)
+        self.assertEqual(ruleset.rules[0].type, "deletion")
+        self.assertEqual(ruleset.rules[1].type, "non_fast_forward")
+        self.assertEqual(ruleset.rules[2].type, "creation")
+        self.assertEqual(len(ruleset.bypass_actors), 1)
+        self.assertEqual(
+            ruleset.bypass_actors[0], {"actor_id": 5, "actor_type": "RepositoryRole", "bypass_mode": "always"}
+        )
+        self.assertEqual(ruleset.current_user_can_bypass, "always")
+        self.assertEqual(
+            ruleset._links,
+            {
+                "self": {"href": "https://api.github.com/repos/PyGithub/PyGithub/rulesets/3490869"},
+                "html": {"href": "https://github.com/PyGithub/PyGithub/rules/3490869"},
+            },
+        )
+
+    def testUpdateRuleset(self):
+        ruleset = self.repo.update_ruleset(
+            3490869,
+            name="updated rule set",
+            target="branch",
+            enforcement="disabled",
+            bypass_actors=[],
+            conditions={
+                "ref_name": {"exclude": [], "include": ["refs/heads/master", "refs/heads/main", "refs/heads/develop"]}
+            },
+            rules=[
+                {"type": "deletion"},
+                {"type": "creation"},
+            ],
+        )
+        self.assertEqual(ruleset.id, 3490869)
+        self.assertEqual(ruleset.name, "updated rule set")
+        self.assertEqual(ruleset.target, "branch")
+        self.assertEqual(ruleset.source_type, "Repository")
+        self.assertEqual(ruleset.source, "PyGithub/PyGithub")
+        self.assertEqual(ruleset.enforcement, "disabled")
+        self.assertEqual(ruleset.node_id, "RRS_lACqUmVwb3NpdG9yec43HleVzgA1RDU")
+        self.assertEqual(ruleset.created_at, datetime.fromisoformat("2025-01-31T20:59:12.515+01:00"))
+        self.assertEqual(ruleset.updated_at, datetime.fromisoformat("2025-01-31T21:09:07.855+01:00"))
+        self.assertEqual(
+            ruleset.conditions,
+            {"ref_name": {"exclude": [], "include": ["refs/heads/master", "refs/heads/main", "refs/heads/develop"]}},
+        )
+        self.assertEqual(len(ruleset.rules), 2)
+        self.assertEqual(ruleset.rules[0].type, "deletion")
+        self.assertEqual(ruleset.rules[1].type, "creation")
+        self.assertEqual(ruleset.bypass_actors, [])
+        self.assertEqual(ruleset.current_user_can_bypass, "never")
+        self.assertEqual(
+            ruleset._links,
+            {
+                "self": {"href": "https://api.github.com/repos/PyGithub/PyGithub/rulesets/3490869"},
+                "html": {"href": "https://github.com/PyGithub/PyGithub/rules/3490869"},
+            },
+        )
+
+    def testDeleteRuleset(self):
+        deleted = self.repo.delete_ruleset(3490869)
+        self.assertTrue(deleted)
 
     def testGetAutomatedSecurityFixes(self):
         self.assertDictEqual(
