9a1fcf29dd3001d18e372bc9b76cc5e67a78286c
Sandra Parsick <sandra@parsick.dev>
2025-09-10 13:17:39 +0200

    refactor: introduce new object for bypass_author
    - based on code review

--- tests/Repository.py
+++ tests/Repository.py
@@ -111,6 +111,7 @@ from unittest import mock
 import github
 import github.Repository
 import github.Requester
+import github.RepositoryRulesetBypassActor
 from github.GithubObject import is_undefined
 
 from . import Framework
@@ -2445,9 +2446,10 @@ class Repository(Framework.TestCase):
             },
         )
         self.assertEqual(len(ruleset.bypass_actors), 1)
-        self.assertEqual(
-            ruleset.bypass_actors[0], {"actor_id": 5, "actor_type": "RepositoryRole", "bypass_mode": "always"}
-        )
+        bypass_actor = ruleset.bypass_actors[0]
+        self.assertEqual(bypass_actor.actor_id, 5)
+        self.assertEqual(bypass_actor.actor_type, "RepositoryRole")
+        self.assertEqual(bypass_actor.bypass_mode, "always")
         self.assertEqual(ruleset.current_user_can_bypass, "always")
         self.assertEqual(
             ruleset._links,
@@ -2492,9 +2494,10 @@ class Repository(Framework.TestCase):
             },
         )
         self.assertEqual(len(ruleset.bypass_actors), 1)
-        self.assertEqual(
-            ruleset.bypass_actors[0], {"actor_id": 5, "actor_type": "RepositoryRole", "bypass_mode": "always"}
-        )
+        bypass_actor = ruleset.bypass_actors[0]
+        self.assertEqual(bypass_actor.actor_id, 5)
+        self.assertEqual(bypass_actor.actor_type, "RepositoryRole")
+        self.assertEqual(bypass_actor.bypass_mode, "always")
         self.assertEqual(ruleset.current_user_can_bypass, "always")
         self.assertEqual(
             ruleset._links,
@@ -2534,9 +2537,11 @@ class Repository(Framework.TestCase):
         self.assertEqual(ruleset.rules[1].type, "non_fast_forward")
         self.assertEqual(ruleset.rules[2].type, "creation")
         self.assertEqual(len(ruleset.bypass_actors), 1)
-        self.assertEqual(
-            ruleset.bypass_actors[0], {"actor_id": 5, "actor_type": "RepositoryRole", "bypass_mode": "always"}
-        )
+        bypass_actor = ruleset.bypass_actors[0]
+        self.assertEqual(bypass_actor.actor_id, 5)
+        self.assertEqual(bypass_actor.actor_type, "RepositoryRole")
+        self.assertEqual(bypass_actor.bypass_mode, "always")
+
         self.assertEqual(ruleset.current_user_can_bypass, "always")
         self.assertEqual(
             ruleset._links,
---
+++ tests/RepositoryRulesetBypassActor.py
@@ -0,0 +1,39 @@
+############################ Copyrights and license ############################
+#                                                                              #
+#                                                                              #
+# This file is part of PyGithub.                                               #
+# http://pygithub.readthedocs.io/                                              #
+#                                                                              #
+# PyGithub is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU Lesser General Public License as published by the Free  #
+# Software Foundation, either version 3 of the License, or (at your option)    #
+# any later version.                                                           #
+#                                                                              #
+# PyGithub is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    #
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more #
+# details.                                                                     #
+#                                                                              #
+# You should have received a copy of the GNU Lesser General Public License     #
+# along with PyGithub. If not, see <http://www.gnu.org/licenses/>.             #
+#                                                                              #
+################################################################################
+
+from __future__ import annotations
+
+from . import Framework
+
+
+class RepositoryRulesetBypassActor(Framework.TestCase):
+    def setUp(self):
+        super().setUp()
+        repo = self.g.get_repo("PyGithub/PyGithub")
+        ruleset = repo.get_ruleset(3474546)
+        self.rrba = ruleset.bypass_actors[0]
+
+
+    def testAttributes(self):
+        rrba = self.rrba
+        self.assertEqual(rrba.actor_id, 5)
+        self.assertEqual(rrba.actor_type, "RepositoryRole")
+        self.assertEqual(rrba.bypass_mode, "always")
