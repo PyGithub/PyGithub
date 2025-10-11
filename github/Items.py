112fa10dbbe47ded1be61cc44fc289281a8e77f0
EnricoMi <github-action-EnricoMi@users.noreply.github.com>
2025-10-11 09:36:51 +0000

    Squashed commit of the following:
    commit 7745a317f3044320aaad473db0f27ffadb6c2f12
    Author: EnricoMi <github-action-EnricoMi@users.noreply.github.com>
    Date:   Sat Oct 11 09:06:00 2025 +0000
    
        Updated test assertions

---
+++ github/Items.py
@@ -0,0 +1,68 @@
+############################ Copyrights and license ############################
+#                                                                              #
+#                                                                              #
+# This file is part of PyGithub.                                               #
+# http://pygithub.readthedocs.io/                                              #
+#                                                                              #
+# PyGithub is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU Lesser General Public License as published by the Free  #
+# Software Foundation, either version 3 of the License, or (at your option)    #
+# any later version.                                                           #
+#                                                                              #
+# PyGithub is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    #
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more #
+# details.                                                                     #
+#                                                                              #
+# You should have received a copy of the GNU Lesser General Public License     #
+# along with PyGithub. If not, see <http://www.gnu.org/licenses/>.             #
+#                                                                              #
+################################################################################
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from github.GithubObject import Attribute, NonCompletableGithubObject, NotSet
+
+if TYPE_CHECKING:
+    from github.GithubObject import NonCompletableGithubObject
+
+
+class Items(NonCompletableGithubObject):
+    """
+    This class represents Items.
+
+    The reference can be found here
+    https://docs.github.com/en/rest
+
+    The OpenAPI schema can be found at
+    - /components/schemas/dependabot-alert-security-advisory/properties/cwes/items
+
+    """
+
+    def _initAttributes(self) -> None:
+        # TODO: remove if parent does not implement this
+        super()._initAttributes()
+        self._cwe_id: Attribute[str] = NotSet
+        self._name: Attribute[str] = NotSet
+
+    def __repr__(self) -> str:
+        # TODO: replace "some_attribute" with uniquely identifying attributes in the dict, then run:
+        return self.get__repr__({"some_attribute": self._some_attribute.value})
+
+    @property
+    def cwe_id(self) -> str:
+        return self._cwe_id.value
+
+    @property
+    def name(self) -> str:
+        return self._name.value
+
+    def _useAttributes(self, attributes: dict[str, Any]) -> None:
+        # TODO: remove if parent does not implement this
+        super()._useAttributes(attributes)
+        if "cwe_id" in attributes:  # pragma no branch
+            self._cwe_id = self._makeStringAttribute(attributes["cwe_id"])
+        if "name" in attributes:  # pragma no branch
+            self._name = self._makeStringAttribute(attributes["name"])
